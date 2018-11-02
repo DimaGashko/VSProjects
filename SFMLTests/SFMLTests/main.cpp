e3625a53ab7ab207f579b68363998fc63ebead72
DimaGashko <dgashko@mail.ua>
2018-11-02 20:32:59 +0200

    baseEvents

--- SFMLTests/SFMLTests/main.cpp
+++ SFMLTests/SFMLTests/main.cpp
@@ -1,22 +1,40 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
+using namespace std;
+
+void events();
+
 int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100, 30);
+	events();
+
+	return 0;
+}
+
+void events() {
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	sf::CircleShape shape(100);
 	shape.setFillColor(sf::Color(255, 255, 0));
 
-	while (window.isOpen())	{
+	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
 				window.close();
+			} 
+			else if (event.type == sf::Event::KeyPressed) {
+				
+				cout << "Pressed (" << event.key.code << ") ";
+			}
+			else if (event.type == sf::Event::KeyReleased) {
+				cout << endl << "Released" << endl;
+			}
+
 		}
 
 		window.clear();
 		window.draw(shape);
 		window.display();
 	}
-
-	return 0;
 }
