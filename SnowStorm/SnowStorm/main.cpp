55ae901fea04f0d78e4a515307aaea6918b48c5d
DimaGashko <dgashko@mail.ua>
2018-11-15 15:22:39 +0200

    edit

--- SnowStorm/SnowStorm/main.cpp
+++ SnowStorm/SnowStorm/main.cpp
@@ -3,22 +3,25 @@
 
 #include "Figure.h"
 
-void controlFigure(snow::Figure &figure);
+void controlFigure(snow::Figure &figure, float frameTime);
 
 int main() {
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 4;
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!", sf::Style::Default, settings);
-	window.setFramerateLimit(30);
+	window.setFramerateLimit(180);
 
 	snow::Figure f1(sf::Vector2f(250, 250), 50, 0.f);
 	snow::Figure f2(sf::Vector2f(50, 250), 20, 0.f);
 	snow::Figure f3(sf::Vector2f(600, 350), 20, 0.f);
 
 	sf::Clock clock;
+	float frameTime;
 
 	while (window.isOpen()) {
+		frameTime = clock.restart().asMilliseconds();
+
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
@@ -30,7 +33,7 @@ int main() {
 			}
 		}
 
-		controlFigure(f1);
+		controlFigure(f2, frameTime);
 
 		window.clear();
 		f1.draw(window);
@@ -42,14 +45,16 @@ int main() {
 	return 0;
 }
 
-void controlFigure(snow::Figure &figure) {
-	float step = 1;
+void controlFigure(snow::Figure &figure, float frameTime) {
+	float step = 2;// frameTime;
+	float angle = 0.01;// frameTime;
+	float scale = 1; /// frameTime;
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		figure.rotate(0.01);
+		figure.rotate(-angle);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		figure.rotate(-0.01);
+		figure.rotate(angle);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
 		figure.move(sf::Vector2f(0, -step));
@@ -64,9 +69,9 @@ void controlFigure(snow::Figure &figure) {
 		figure.move(sf::Vector2f(step, 0));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		figure.setSize(figure.getSize() + 0.1);
+		figure.setSize(figure.getSize() + scale);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-		figure.setSize(figure.getSize() - 0.1);
+		figure.setSize(figure.getSize() - scale);
 	}
 }
