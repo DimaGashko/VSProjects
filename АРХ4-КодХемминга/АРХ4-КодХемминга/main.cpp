97c74ae65247f08dc7a7f40fc41b5ef86ade677f
DimaGashko <dgashko@mail.ua>
2018-11-22 01:39:00 +0200

    init

---
+++ АРХ4-КодХемминга/АРХ4-КодХемминга/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(866);
+	SetConsoleOutputCP(866);
+		
+	cout << '|' << char(14) << char(4) << '|' << endl;
+	cout << '|' << char(14) << char(0) << '|' << endl;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
