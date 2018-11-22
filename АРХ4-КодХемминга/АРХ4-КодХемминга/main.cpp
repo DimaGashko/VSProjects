ea2727b947ac0d18435810abd92eca63d3cadf9e
DimaGashko <dgashko@mail.ua>
2018-11-22 02:41:39 +0200

    changeToVector

--- АРХ4-КодХемминга/АРХ4-КодХемминга/main.cpp
+++ АРХ4-КодХемминга/АРХ4-КодХемминга/main.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
 #include <windows.h>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-string decodeHamming(string word);
+vector<bool> decodeHamming(vector<bool> word);
+void printWord(vector<bool> word);
 
 int main() {
 	SetConsoleCP(866);
@@ -13,29 +14,57 @@ int main() {
 	cout << '|' << char(14) << char(4) << '|' << endl;
 	cout << '|' << char(14) << char(0) << '|' << endl;
 
-	auto res = decodeHamming("0010111101");
-	cout << res << endl;
+	vector<bool> input = { 0,0,1,0,1,1,1,1,0,1 };
+	auto output = decodeHamming(input);
+
+	printWord(input);
+	printWord(output);
 
 	cout << endl;
 	system("pause");
 	return 0;
 }
 
-string decodeHamming(string word) {
-	int size = word.length();
+vector<bool> decodeHamming(vector<bool> word) {
+	int size = word.size();
+
 	if (size < 3) {
 		cerr << "Code word have to have leat 3 bits";
-		return "";
+		return word;
+	}
+	
+	short errors = 0; //Количество несовпавших проверочных битов
+	int errIndex = -1; //Индекс бита с ошибкой
+
+	/*for (int r = 1; r <= size; r *= 2) {
+		int res = 0;
+
+		for (int i = r; r <= size;) {
+			
+		}
+
+		if (res != 0) {
+			errors++;
+			errIndex += r;
+		}
 	}
 
-	int errIndex = -1;
-
+	if (errors >= 2) {
+		word[errIndex] = !word[errIndex];
+	}
+	else if (errors == 1) {
+		//Error in parity bit
+	}*/
 
 	return word;
 }
 
+void printWord(vector<bool> word) {
+	for (int i = 0; i < word.size(); i++) {
+		cout << (int)word[i];
+	}
 
-
-
+	cout << endl;
+}
 
 
