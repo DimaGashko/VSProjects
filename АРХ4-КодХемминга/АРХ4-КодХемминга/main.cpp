a29c388ded006eceadce6e681280b2f5c04915cd
DimaGashko <dgashko@mail.ua>
2018-11-22 02:14:20 +0200

    sizeCheck

--- АРХ4-КодХемминга/АРХ4-КодХемминга/main.cpp
+++ АРХ4-КодХемминга/АРХ4-КодХемминга/main.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <string>
 
 using namespace std;
+
+string decodeHamming(string word);
 
 int main() {
 	SetConsoleCP(866);
@@ -10,7 +13,29 @@ int main() {
 	cout << '|' << char(14) << char(4) << '|' << endl;
 	cout << '|' << char(14) << char(0) << '|' << endl;
 
+	auto res = decodeHamming("0010111101");
+	cout << res << endl;
+
 	cout << endl;
 	system("pause");
 	return 0;
 }
+
+string decodeHamming(string word) {
+	int size = word.length();
+	if (size < 3) {
+		cerr << "Code word have to have leat 3 bits";
+		return "";
+	}
+
+	int errIndex = -1;
+
+
+	return word;
+}
+
+
+
+
+
+
