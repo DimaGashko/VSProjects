79446f6d2fb486d83d3c578384d27d7d33861554
DimaGashko <dgashko@mail.ua>
2018-12-04 14:59:32 +0200

    correctGetArrTarg

--- АРХ5-Cash/АРХ5-Cash/main.cpp
+++ АРХ5-Cash/АРХ5-Cash/main.cpp
@@ -9,7 +9,7 @@ const int MIN_LEN = 1024/4; //256
 const int MAX_LEN = 32*1024*1024/4; //8388608
 
 //Возвращает массив для тестирования с размером len заполненный type способом
-//type = "preorder" - прямой | "postorder" - обратный | "rand" 
+//type = "preorder" - прямой | "postorder" - обратный | "randorder" 
 int* getTargArr(int len, string type) {
 	int *arr = new int[len];
 
@@ -20,17 +20,33 @@ int* getTargArr(int len, string type) {
 	if (type == "postorder") {
 		reverse(arr, arr + len);
 	}
-	else if (type == "rand") {
+	else if (type == "randorder") {
 		std::random_shuffle(arr, arr + len);
 	}
 
 	return arr;
 }
 
+void printArr(int *arr, int len) {
+	for (int i = 0; i < len; i++) {
+		cout << arr[i] << ' ';
+	}
+
+	cout << endl;
+}
+
 int main() {
 	srand(time(0));
+	const int len = 20;
+
+	auto arr1 = getTargArr(len, "preorder");
+	auto arr2 = getTargArr(len, "postorder");
+	auto arr3 = getTargArr(len, "randorder");
 	
-	
+	printArr(arr1, len);
+	printArr(arr2, len);
+	printArr(arr3, len);
+
 	system("pause");
 	return 0;
 }
