cdc7ebbd0ed81a84a5558f58492a2ecfa8eab230
DimaGashko <dgashko@mail.ua>
2019-02-20 18:29:26 +0200

    edit

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,27 +1,23 @@
-// https://www.e-olymp.com/ru/submissions/5114556
-
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
+	int n, k;
+	cin >> n >> k;
 
-	int x1, y1, x2, y2, x3, y3, x4, y4, w, h;
-	int pl, pt, pr, pb, fullW, fullH;
+	float base = ceil(float(n) / k) * 4 - 2;
 
-	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4 >> w >> h;
+	cout << base;
 
-	pl = abs(x3 - x1);
-	pt = abs(y2 - y4);
-	pr = abs(x2 - x4);
-	pb = abs(y3 - y1);
-	fullW = abs(x2 - x1);
-	fullH = abs(y2 - y1);
+	/*if ((n% k) > k / 2) {
+		cout << base - 2;
+	}
+	else {
+		cout << base;
+	}*/
 
-	bool ok = w <= fullW && h <= fullH && (w <= pl || w <= pr || h <= pt || h <= pb);
-
-	cout << (ok ? "Yes" : "No") << endl;
 
 	system("pause");
 	return 0;
