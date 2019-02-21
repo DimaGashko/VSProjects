3b2ea072b175138bc7f9f0fff8f7f595f7f618e0
DimaGashko <dgashko@mail.ua>
2019-02-21 18:45:25 +0200

    front/back

--- List-semester4-algorithms-lab2/List-semester4-algorithms-lab2/List.cpp
+++ List-semester4-algorithms-lab2/List-semester4-algorithms-lab2/List.cpp
@@ -2,12 +2,22 @@
 
 namespace dg {
 
-	List::List() {
+	List::List():
+		m_size(0)
+	{
 
 	}
 
+	List::Node* List::front() {
+		return m_head.next;
+	}
+
+	List::Node* List::back() {
+		return m_back;
+	}
 
 	List::~List() {
+
 	}
 
 }; // namespace dg
--- List-semester4-algorithms-lab2/List-semester4-algorithms-lab2/List.hpp
+++ List-semester4-algorithms-lab2/List-semester4-algorithms-lab2/List.hpp
@@ -1,11 +1,42 @@
-#pragma once
+﻿#pragma once
 
 namespace dg {
 
 	class List {
 	public:
 		List();
+
+		struct Node {
+			int value;
+			Node* next;
+			Node* prev;
+		};
+
+		Node* front();
+		Node* back();
+
+		// Удаляет первый элемент из списка и возвращает его
+		Node* popFront();
+
+		// Удаляет последний элемент из списка и возвращает его
+		Node* popBack();
+
+		void pushFront();
+		void pushBack();
+
+		Node* getByIndex();
+
+		void clear();
+
 		~List();
+
+	private:
+
+		Node m_head; // Не указатель
+		Node* m_back;
+
+		int m_size;
+
 	};
 
 }; // namespace dg
