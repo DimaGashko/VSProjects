b82789c125f79335aa06147f392ebd95b218053e
DimaGashko <dgashko@mail.ua>
2019-02-23 11:07:34 +0200

    findError

--- List-semester4-algorithms-lab2/List-semester4-algorithms-lab2/main.cpp
+++ List-semester4-algorithms-lab2/List-semester4-algorithms-lab2/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream> // 2/21/2019 5:37 pm
+#include <iostream>
 
 #include "List.hpp"
 
@@ -21,7 +21,7 @@ void writeList(dg::List &list) {
 	std::cout << "Enter items of the list" << std::endl;
 	std::cout << "(enter no number to complete the input)" << std::endl;
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 2; i++) {
 		std::cin >> nextValue;
 		list.pushBack(nextValue);
 	}
