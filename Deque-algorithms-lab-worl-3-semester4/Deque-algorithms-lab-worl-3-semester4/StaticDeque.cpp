804688488d45d0e3ddbc732b93ab4762be11fd61
DimaGashko <dgashko@mail.ua>
2019-02-23 16:29:07 +0200

    pushFront/pushBack

--- Deque-algorithms-lab-worl-3-semester4/Deque-algorithms-lab-worl-3-semester4/StaticDeque.cpp
+++ Deque-algorithms-lab-worl-3-semester4/Deque-algorithms-lab-worl-3-semester4/StaticDeque.cpp
@@ -9,13 +9,49 @@ namespace dg {
 
 	}
 
+	int StaticDeque::front() {
+		return m_front;
+	}
+
+	int StaticDeque::back() {
+		return m_back;
+	}
+
+	void StaticDeque::pushFront(int value) {
+		_checkCanAdd();
+
+		if (m_size != 0) {
+			m_front = _toIndex(m_front - 1);
+		}
+		
+		m_items[m_front] = value;
+	}
+
+	void StaticDeque::pushBack(int value) {
+		_checkCanAdd();
+
+		if (m_size != 0) {
+			m_front = _toIndex(m_front + 1);
+		}
+
+		m_items[m_front] = value;
+	}
+
 	int StaticDeque::size() {
 		return m_size;
 	}
 
+	inline
 	int StaticDeque::_toIndex(int index) {
 		if (index < 0) index = m_capacity + index;
 		return index % m_capacity;
+	}
+
+	inline
+	void StaticDeque::_checkCanAdd() {
+		if (m_size >= m_capacity - 1) return;
+
+		std::runtime_error("Capacity overflow");
 	}
 
 	StaticDeque::~StaticDeque() {
