c01b6a2d04f79afdb123bb4fe94dd52f19b845c3
DimaGashko <dgashko@mail.ua>
2019-02-24 19:12:59 +0200

    edit

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,82 +1,56 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <queue>
+#include <list>
+#include <vector>
 
 using namespace std;
 
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
-class QArr {
-public:
-	QArr(int n):
-		m_n(n),
-		m_arr(new long long[m_n])
-	{
-		for (int i = 0; i < m_n; i++) {
-			m_arr[i] = 0;
-		}
-	}
+void processTest();
 
-	void q_0(int p, int q, long long v) {
-		if (v == 0) return;
-		if (p > q) return q_0(q, p, v);
+struct command0 {
+	int p, q, v;
+};
 
-		for (int i = p; i <= q; i++) {
-			m_arr[i] += v;
-		}
-	}
-	
-	long long q_1(int p, int q) {
-		if (p > q) return q_1(q, p);
-
-		long long sum = 0;
-
-		for (int i = p; i <= q; i++) {
-			sum += m_arr[i];
-		}
-
-		return sum;
-	}
-
-	~QArr() {
-		delete m_arr;
-	}
-
-private:
-	
-	int m_n;
-	long long* m_arr;
+struct command1 {
+	int p, v;
 };
 
 int main() {
-	int t, n, c;
+	queue<int[2]> q;
 
+	q.push({ 1, 2 });
+	fout << q.front()[0] << " " << q.front()[1];// << q.front()[2];
+
+	return 0;
+	int t, n;
 	fin >> t;
 
 	for (int i = 0; i < t; i++) {
-		fin >> n >> c;
+		fin >> n;
 
-		QArr qArr(n);
-
-		for (int i = 0; i < c; i++) {
-			int command, p, q;
-			fin >> command >> p >> q;
-
-			p--; q--;
-
-			if (command == 0) {
-				int v;
-				fin >> v;
-
-				qArr.q_0(p, q, v);
-			}
-			else {
-				fout << qArr.q_1(p, q) << endl;
-			}
-		}0
-
+		processTest();
 	}
 
 	return 0;
+}
+
+void processTest() {
+	int c; 
+	fin >> c;
+
+	queue<int> commands;
+	queue<command0> cs0;
+	list<command1> cs1;
+
+	for (int i = 0; i < c; i++) {
+		int command;
+		fin >> command;
+		commands.push(command);
+		
+
+	}
 }
