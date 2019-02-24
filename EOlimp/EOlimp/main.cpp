fddfd23db7e1fa71f93115de628329cd91443b2c
DimaGashko <dgashko@mail.ua>
2019-02-24 14:40:09 +0200

    newTaskBase

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,21 +1,77 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
+class QArr {
+public:
+	QArr(int n):
+		m_n(n),
+		m_arr(new double[m_n])
+	{
+		initArr();
+	}
+
+	void q_0(int p, int q, double v) {
+		for (int i = p; i <= q; i++) {
+			m_arr[i] += v;
+		}
+	}
+
+	double q_1(int p, int q) {
+		double sum = 0;
+
+		for (int i = p; i <= q; i++) {
+			sum += m_arr[i];
+		}
+
+		return sum;
+	}
+
+	~QArr() {
+		delete[] m_arr;
+	}
+private:
+
+	int m_n;
+	double* m_arr;
+
+	void initArr() {
+		for (int i = 0; i < m_n; i++) {
+			m_arr[i] = 0;
+		}
+	}
+};
+
 int main() {
-	int n, k;
-	cin >> n >> k;
+	int t, n, c;
+	string res;
 
-	float base = ceil(float(n) / k) * 4;
+	cin >> t;
 
-	if ((n % k) % 2 == 0 || (n / k) % 2 == 0) {
-		cout << base;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> c;
+
+		QArr qArr(n);
+
+		for (int i = 0; i < c; i++) {
+			int command, p, q;
+			cin >> command >> p >> q;
+
+			if (command == 0) {
+				double v;
+				cin >> v;
+
+				qArr.q_0(p, q, v);
+			}
+			else {
+				res += to_string(qArr.q_1(p, q)) + "\n";
+			}
+		}
+
 	}
-	else {
-		cout << base - 2;
-	}
 
-	system("pause");
+	cout << res;
+
 	return 0;
 }
