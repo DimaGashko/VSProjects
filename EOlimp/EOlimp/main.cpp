714c89c6c0d7c950d237004454527c5420e0fbc9
DimaGashko <dgashko@mail.ua>
2019-02-25 22:42:59 +0200

    done

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -10,55 +10,23 @@ ofstream fout("output.txt");
 typedef vector<vector<bool>> G;
 
 void writeG();
-void run(int v = 0, int prev = -1);
-void printResult();
+void printRes();
 
 G g;
-G tree;
-vector<bool> vs;
-
 int n = 0, m = 0;
 
 int main() {
 
 	writeG();
-	run();
-	printResult();
+	printRes();
 
 	return 0;
-}
-
-void run(int v, int prev) {
-	if (vs[v]) return;
-	vs[v] = true;
-
-	if (prev != -1) {
-		tree[v][prev] = 1;
-		tree[prev][v] = 1;
-	}
-
-	for (int i = 0; i < n; i++) {
-		if (g[v][i] == 0) continue;
-
-		run(i, v);
-	}
-}
-
-void printResult() {
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (tree[i][j] == 0) continue;
-			fout << i + 1 << " " << j + 1 << endl;
-		}
-	}
 }
 
 void writeG() {
 	fin >> n >> m;
 
 	g = G(n, vector<bool>(n));
-	tree = G(n, vector<bool>(n));
-	vs = vector<bool>(n);
 
 	for (int i = 0; i < m; i++) {
 		int a, b;
@@ -66,7 +34,15 @@ void writeG() {
 		a--; b--;
 
 		g[a][b] = 1;
-		g[b][a] = 1;
 	}
+}
 
+void printRes() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j != 0) fout << " ";
+			fout << g[i][j];
+		}
+		fout << endl;
+	}
 }
