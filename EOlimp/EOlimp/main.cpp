f6664b2bc365a296e91267f4a2a031897da59261
DimaGashko <dgashko@mail.ua>
2019-02-25 10:27:04 +0200

    baseSlutions

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -4,38 +4,77 @@
 
 using namespace std;
 
-typedef vector<vector<int>> G;
+typedef vector<vector<bool>> G;
 
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
-void writeGraph(G &g);
+void writeGraph();
+void writeVs();
+void run(int v);
+void checkRes();
+
+G g;
+vector<bool> vs;
+int n = 0;
+bool isLoop = false;
 
 int main() {
-
-	G g;
-	writeGraph(g);
-
-	for (auto& row : g) {
-		for (auto& item : row) {
-			fout << item << " ";
-		}
-
-		fout << endl;
-	}
+	writeGraph();
+	writeVs();
+	run(0);
+	checkRes();
 	
 	return 0;
 }
 
-void writeGraph(G &g) {
-	int n = 0;
+void run(int v) {
+	if (isLoop) return;
+
+	if (vs[v] == true) {
+		isLoop = true;
+		return;
+	}
+
+	vs[v] = true;
+
+	for (int i = 0; i < n; i++) {
+		if (g[v][i] == 0) continue;
+
+		run(i);
+	}
+}
+
+void checkRes() {
+	bool isTree = true;
+
+	for (auto item : vs) {
+		if (item) continue;
+
+		isTree = false;
+		break;
+	}
+
+	fout << (isTree ? "YES" : "NO");
+}
+
+void writeGraph() {
 	fin >> n;
 
-	g = G(n, vector<int>(n));
+	g = G(n, vector<bool>(n));
 
-	for (auto &row : g) {
-		for (auto &item : row) {
-			fin >> item;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			bool val;
+			fin >> val;
+
+			if (j <= i) continue;
+
+			g[i][j] = val;
 		}
 	}
+}
+
+void writeVs() {
+	vs = vector<bool>(g.size());
 }
