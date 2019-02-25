da306e475c06cc717dacf0cd87400782c71bec09
DimaGashko <dgashko@mail.ua>
2019-02-25 12:40:49 +0200

    done

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -11,6 +11,7 @@ typedef vector<vector<bool>> G;
 
 void writeG();
 void run(int v = 0, int prev = -1);
+void printResult();
 
 G g;
 G tree;
@@ -22,14 +23,7 @@ int main() {
 
 	writeG();
 	run();
-
-	for (auto row : tree) {
-		for (auto item : row) {
-			fout << item << " ";
-		}
-
-		fout << endl;
-	}
+	printResult();
 
 	return 0;
 }
@@ -47,6 +41,15 @@ void run(int v, int prev) {
 		if (g[v][i] == 0) continue;
 
 		run(i, v);
+	}
+}
+
+void printResult() {
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (tree[i][j] == 0) continue;
+			fout << i + 1 << " " << j + 1 << endl;
+		}
 	}
 }
 
