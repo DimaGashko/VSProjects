2c16459023f0ee53937f0db04898fca8f501ddd8
DimaGashko <dgashko@mail.ua>
2019-02-25 12:29:33 +0200

    baseSolution

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -10,8 +10,10 @@ ofstream fout("output.txt");
 typedef vector<vector<bool>> G;
 
 void writeG();
+void run(int v = 0, int prev = -1);
 
 G g;
+G tree;
 vector<bool> vs;
 
 int n = 0, m = 0;
@@ -19,14 +21,40 @@ int n = 0, m = 0;
 int main() {
 
 	writeG();
+	run();
+
+	for (auto row : tree) {
+		for (auto item : row) {
+			fout << item << " ";
+		}
+
+		fout << endl;
+	}
 
 	return 0;
+}
+
+void run(int v, int prev) {
+	if (vs[v]) return;
+	vs[v] = true;
+
+	if (prev != -1) {
+		tree[v][prev] = 1;
+		tree[prev][v] = 1;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (g[v][i] == 0) continue;
+
+		run(i, v);
+	}
 }
 
 void writeG() {
 	fin >> n >> m;
 
 	g = G(n, vector<bool>(n));
+	tree = G(n, vector<bool>(n));
 	vs = vector<bool>(n);
 
 	for (int i = 0; i < m; i++) {
