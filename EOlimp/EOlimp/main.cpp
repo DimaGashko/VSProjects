e74ebbc589ba9e2ac658e8da82d71adcb9f8d0e1
DimaGashko <dgashko@mail.ua>
2019-02-25 10:04:22 +0200

    edit

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,56 +1,41 @@
 #include <iostream>
 #include <fstream>
-#include <queue>
-#include <list>
 #include <vector>
 
 using namespace std;
 
+typedef vector<vector<int>> G;
+
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
-void processTest();
-
-struct command0 {
-	int p, q, v;
-};
-
-struct command1 {
-	int p, v;
-};
+void writeGraph(G &g);
 
 int main() {
-	queue<int[2]> q;
 
-	q.push({ 1, 2 });
-	fout << q.front()[0] << " " << q.front()[1];// << q.front()[2];
+	G g;
+	writeGraph(g);
 
-	return 0;
-	int t, n;
-	fin >> t;
+	for (auto& row : g) {
+		for (auto& item : row) {
+			fout << item << " ";
+		}
 
-	for (int i = 0; i < t; i++) {
-		fin >> n;
-
-		processTest();
+		fout << endl;
 	}
-
+	
 	return 0;
 }
 
-void processTest() {
-	int c; 
-	fin >> c;
+void writeGraph(G &g) {
+	int n = 0;
+	fin >> n;
 
-	queue<int> commands;
-	queue<command0> cs0;
-	list<command1> cs1;
+	g = G(n, vector<int>(n));
 
-	for (int i = 0; i < c; i++) {
-		int command;
-		fin >> command;
-		commands.push(command);
-		
-
+	for (auto &row : g) {
+		for (auto &item : row) {
+			fin >> item;
+		}
 	}
 }
