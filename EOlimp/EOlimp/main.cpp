9e29ecda0be9146e4adb100ed556891ee8f7be5b
DimaGashko <dgashko@mail.ua>
2019-02-26 19:15:26 +0200

    writeG

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -15,12 +15,33 @@ int n = 0;
 int res = 0;
 
 void writeG() {
+	int m;
+	fin >> n >> m;
 
+	g = G(n, vector<int>(n));
+	vs = vector<bool>(n);
+
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		fin >> a >> b >> c;
+		a--; b--;
+
+		g[a][b] = c;
+		g[b][a] = c;
+	}
 }
 
 int main() {
 
 	writeG();
+
+	for (auto row : g) {
+		for (auto item : row) {
+			fout << item << " ";
+		}
+
+		fout << endl;
+	}
 
 	return 0;
 }
