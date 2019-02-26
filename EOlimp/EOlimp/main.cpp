5fe64f392c66f9e8f8fbf7933973ac09f9ff8df5
DimaGashko <dgashko@mail.ua>
2019-02-26 21:41:32 +0200

    done

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
-#include <set>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
+	double x1, y1, x2, y2, x3, y3, x4, y4;
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
 
-	if (a > b) swap(a, b);
-	if (c > d) swap(c, d);
-
-	set<int> muls;
-
-	for (int i = a; i <= b; i++) {
-		for (int j = c; j <= d; j++) {
-			muls.insert(i * j);
-		}
-	}
-
-	cout << muls.size() << endl;
+	cout << round(0.5 * abs(x1 * y2 + x2 * y3 + x3 * y4 + x4 * y1 - x2 * y1 - x3 * y2 - x4 * y3 - x1 * y4)) << endl;
 
 	system("pause");
 	return 0;
