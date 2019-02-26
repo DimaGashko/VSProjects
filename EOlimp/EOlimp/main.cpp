3468cef6ed938df630bb1108b5ad8fe4cca14fb4
DimaGashko <dgashko@mail.ua>
2019-02-26 10:07:26 +0200

    getGrapgh

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -10,12 +11,13 @@ ofstream fout("output.txt");
 typedef vector<vector<bool>> G;
 
 G g;
+int gN = 0;
 
 void writeG() {
 	int m, n;
 	fin >> m >> n;
 
-	vector<string> map(m);
+	vector<string> strMap(m);
 
 	for (int i = 0; i < m; i++) {
 		string row;
@@ -25,21 +27,47 @@ void writeG() {
 			continue;
 		}
 
-		map[i] = row;
+		strMap[i] = row;
 	}
+
+	map<pair<int, int>, int> vsIndexes;
+
+	for (int i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) {
+			if (strMap[i][j] == '.') continue;
+			gN++;
+			vsIndexes[pair<int, int>(i, j)] = gN;
+		}
+	}
+
+	g = G(gN, vector<bool>(gN));
 
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
-			fout << map[i][j];
-		}
+			if (strMap[i][j] == '.') continue;
 
-		fout << endl;
+			int aIndex = vsIndexes[pair<int, int>(i, j)] - 1;
+			int lIndex = vsIndexes[pair<int, int>(i, j - 1)] - 1;
+			int tIndex = vsIndexes[pair<int, int>(i - 1, j)] - 1;
+			int rIndex = vsIndexes[pair<int, int>(i, j + 1)] - 1;
+			int bIndex = vsIndexes[pair<int, int>(i + 1, j)] - 1;
+
+			if (lIndex != -1) g[aIndex][lIndex] = 1;
+			if (tIndex != -1) g[aIndex][tIndex] = 1;
+			if (rIndex != -1) g[aIndex][rIndex] = 1;
+			if (bIndex != -1) g[aIndex][bIndex] = 1;
+		}
 	}
+
+}
+
+void run(int v = 0, int prev = -1) {
+
 }
 
 int main() {
-
 	writeG();
+	run();
 
 	return 0;
 }
