6236013921aa0df1143f59f74451d87a9abb9532
DimaGashko <dgashko@mail.ua>
2019-02-26 09:22:33 +0200

    readDone

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -12,17 +12,28 @@ typedef vector<vector<bool>> G;
 G g;
 
 void writeG() {
-	int n, m;
-	fin >> n >> m;
+	int m, n;
+	fin >> m >> n;
 
 	vector<string> map(m);
 
 	for (int i = 0; i < m; i++) {
-		fin >> map[i];
+		string row;
+		fin >> row;
+
+		if (row.empty()) {
+			continue;
+		}
+
+		map[i] = row;
 	}
 
-	for (auto row : map) {
-		fout << row << endl;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			fout << map[i][j];
+		}
+
+		fout << endl;
 	}
 }
 
