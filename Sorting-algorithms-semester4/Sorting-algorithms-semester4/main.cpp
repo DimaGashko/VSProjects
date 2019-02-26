49b7494a3d8dc6c2e6f5c438452c491e1e2d6277
DimaGashko <dgashko@mail.ua>
2019-02-26 23:27:05 +0200

    basa

--- Sorting-algorithms-semester4/Sorting-algorithms-semester4/main.cpp
+++ Sorting-algorithms-semester4/Sorting-algorithms-semester4/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <ctime>
+
+void selectionSort(std::vector<int>& arr);
+
+void printArr(std::vector<int>& arr);
+void writeVector(std::vector<int>& arr);
 
 int main() {
-	
+	srand((int)time(0));
 
+	std::vector<int> arr(20);
+	writeVector(arr);
+
+	printArr(arr);
+
+	system("pause");
 	return 0;
+}
+
+void selectionSort(std::vector<int>& arr) {
+
+}
+
+void writeVector(std::vector<int>& arr) {
+	for (auto &item : arr) {
+		item = (rand() % 89) + 10;
+	}
+}
+
+void printArr(std::vector<int>& arr) {
+	for (auto item : arr) {
+		std::cout << item << " ";
+	}
+
+	std::cout << std::endl;
 }
