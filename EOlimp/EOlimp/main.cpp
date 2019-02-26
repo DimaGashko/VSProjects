eda340607819d6f20e6cb29a68a5a2a251811df3
DimaGashko <dgashko@mail.ua>
2019-02-26 23:04:36 +0200

    dne

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -4,10 +4,15 @@
 using namespace std;
 
 int main() {
-	double x1, y1, x2, y2, x3, y3, x4, y4;
-	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+	int a, b, x1, y1;
+	cin >> a >> b >> x1 >> y1;
 
-	cout << round(0.5 * abs(x1 * y2 + x2 * y3 + x3 * y4 + x4 * y1 - x2 * y1 - x3 * y2 - x4 * y3 - x1 * y4)) << endl;
+	float s1 = abs(b * a);
+	float s2 = abs((y1 - b) * a);
+	float s3 = abs((y1 - b) * (x1 - a));
+	float s4 = abs(b * (x1 - a));
+
+	printf("%.3f %.3f %.3f %.3f\n", s1, s2, s3, s4);
 
 	system("pause");
 	return 0;
