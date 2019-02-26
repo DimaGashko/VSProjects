998562b356054f59e33c3bf431f86a1b3806105f
DimaGashko <dgashko@mail.ua>
2019-02-26 20:32:22 +0200

    nextNode

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -1,72 +1,25 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
+#include <set>
 
 using namespace std;
 
-ifstream fin("input.txt");
-ofstream fout("output.txt");
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
 
-typedef vector<vector<int>> G;
+	if (a > b) swap(a, b);
+	if (c > d) swap(c, d);
 
-G g;
-vector<bool> vs;
-int n = 0;
-int vUsed = 1;
-int resW = 0;
+	set<int> muls;
 
-void writeG() {
-	int m;
-	fin >> n >> m;
-
-	g = G(n, vector<int>(n));
-	vs = vector<bool>(n);
-
-	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		fin >> a >> b >> c;
-		a--; b--;
-
-		g[a][b] = c;
-		g[b][a] = c;
-	}
-}
-
-void next() {
-	int index = -1;
-	int minW = 1e10;
-
-	for (int v = 1; v < n; v++) {
-		if (vs[v] == true) continue;
-
-		for (int i = 0; i < n; i++) {
-			if (vs[i] == false) continue;
-			int w = g[v][i];
-
-			if (w == 0 || w >= minW) continue;
-
-			index = v;
-			minW = w;
+	for (int i = a; i <= b; i++) {
+		for (int j = c; j <= d; j++) {
+			muls.insert(i * j);
 		}
 	}
 
-	if (index == -1) return;
+	cout << muls.size() << endl;
 
-	vs[index] = true;
-	resW += minW;
-	vUsed++;
-}
-
-int main() {
-	writeG();
-
-	vs[0] = true;
-
-	while (vUsed < n) {
-		next();
-	}
-	
-	fout << resW;
-
+	system("pause");
 	return 0;
 }
