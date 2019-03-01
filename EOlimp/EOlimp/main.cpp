b99c3d402bcf545ced285eda3630974fc0944e97
DimaGashko <dgashko@mail.ua>
2019-03-01 18:19:53 +0200

    fixBySearch

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -28,7 +28,7 @@ vector<int> getPrimes(int n) {
 bool hasItem(vector<int> arr, int item, int l, int r) {
 	if (l >= r) return arr[l] == item;
 
-	int middle = (r - l) / 2;
+	int middle = (l + r) / 2;
 
 	if (arr[middle] > item) return hasItem(arr, item, l, middle - 1);
 	if (arr[middle] < item) return hasItem(arr, item, middle + 1, r);
@@ -44,64 +44,23 @@ int getReversed(int n) {
 
 int main() {
 	int a, b;
-	//cin >> a >> b;
+	cin >> a >> b;
 
-	//if (a > b) swap(a, b);
+	if (a > b) swap(a, b);
 
-	vector<int> d;
-
-	d.push_back(1);
-	d.push_back(3);
-	d.push_back(5);
-	d.push_back(7);
-	d.push_back(9);
-	d.push_back(12);
-	d.push_back(13);
-	d.push_back(16);
-	d.push_back(17);
-	d.push_back(19);
-	d.push_back(21);
-	d.push_back(23);
-	d.push_back(29);
-	d.push_back(31);
-	d.push_back(32);
-	d.push_back(58);
-	d.push_back(66);
-	d.push_back(73);
-	d.push_back(85);
-
-	cout << (hasItem(d, 1, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 3, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 3, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 7, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 9, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 12, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 13, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 16, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 17, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 19, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 21, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 23, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 29, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 31, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 32, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 58, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 66, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 73, 0, d.size() - 1) ? "YES" : "NO") << endl;
-	cout << (hasItem(d, 85, 0, d.size() - 1) ? "YES" : "NO") << endl;
-
-	/*
 	auto primes = getPrimes(10000);
 	int mirrorPrimesCount = 0;
 	int primesSize = primes.size();
 
-	for (auto prime : primes) {
-		if (hasItem(primes, getReversed(prime), 0, primesSize - 1)) {
+	for (int i = a; i <= b; i++) {
+		cout << primes[i] << " " << getReversed(primes[i]) << hasItem(primes, getReversed(primes[i]), 0, primesSize - 1);
+
+		if (hasItem(primes, getReversed(primes[i]), 0, primesSize - 1)) {
 			mirrorPrimesCount++;
 		}
 	}
 
-	cout << mirrorPrimesCount << endl;*/
+	cout << mirrorPrimesCount << endl;
 
 	system("pause");
 	return 0;
