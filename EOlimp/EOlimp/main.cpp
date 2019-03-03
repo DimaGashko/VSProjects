f6bc3b1d04329b8f012fbbeb7d37b55bc2e8a1e4
DimaGashko <dgashko@mail.ua>
2019-03-03 21:06:20 +0200

    readData

--- EOlimp/EOlimp/main.cpp
+++ EOlimp/EOlimp/main.cpp
@@ -11,20 +11,44 @@ struct Pos {
 int n;
 Pos start;
 Pos goal;
-vector<string> taksMap;
+vector<string> taskMap;
 vector<vector<bool>> visited;
 
 vector<Pos> currentWay;
 vector<Pos> minWay;
 
 void init() {
+	cin >> n;
 
+	taskMap = vector<string>(n);
+	visited = vector<vector<bool>>(n, vector<bool>(n));
+
+	for (int i = 0; i < n; i++) {
+		cin >> taskMap[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			auto item = taskMap[i][j];
+
+			if (item != '@' && item != 'X') {
+				continue;
+			}
+
+			Pos pos = { i, j };
+
+			if (item == '@') start = pos;
+			else if (item == 'X') goal = pos;
+
+			taskMap[i][j] = '.';
+		}
+	}
 }
 
 bool isEmpty(Pos pos) {
 	if (pos.x < 0 || pos.y < 0 || pos.x >= n || pos.y >= n) return false;
 
-	return taksMap[pos.x][pos.y] == '.';
+	return taskMap[pos.x][pos.y] == '.';
 }
 
 void nextStep(Pos pos) {
@@ -32,7 +56,7 @@ void nextStep(Pos pos) {
 }
 
 void printResult() {
-
+	
 }
 
 int main() {
