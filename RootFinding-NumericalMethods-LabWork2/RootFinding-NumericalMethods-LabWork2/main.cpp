15df11d956f6ecb5d873214bb3db4eaf8353ec9a
DimaGashko <dgashko@mail.ua>
2019-03-06 00:53:41 +0200

    bisection

--- RootFinding-NumericalMethods-LabWork2/RootFinding-NumericalMethods-LabWork2/main.cpp
+++ RootFinding-NumericalMethods-LabWork2/RootFinding-NumericalMethods-LabWork2/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <cmath>
+
+const double DEF_EPSILON = 0.0001;
+
+double getF(double x) {
+	return sin(x);
+}
+
+double bisection(double l, double r, double eps = DEF_EPSILON);
 
 int main() {
 
+	while (true) {
 
+		double l, r;
+		std::cin >> l >> r;
+
+		std::cout << bisection(l, r) << std::endl;
+
+	}
 
 	system("pause");
 	return 0;
+}
+
+double bisection(double l, double r, double eps) {
+	double middle = getF((l + r) / 2);
+	if (abs(middle) <= eps) return middle;
+
+	if (middle * l < 0) return bisection(l, middle);
+	else return bisection(middle, r);
 }
