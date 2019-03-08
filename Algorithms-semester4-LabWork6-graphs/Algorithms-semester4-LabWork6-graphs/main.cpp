81a681d24b989da92511d0e35788d5dc849a50d1
DimaGashko <dgashko@mail.ua>
2019-03-08 14:53:15 +0200

    initLoop

--- Algorithms-semester4-LabWork6-graphs/Algorithms-semester4-LabWork6-graphs/main.cpp
+++ Algorithms-semester4-LabWork6-graphs/Algorithms-semester4-LabWork6-graphs/main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <list>
+
+void printHello();
 
 template <typename T>
 T prompt(const char label[]);
 
 int main() {
+	printHello();
 
+	while (true) {
+		std::cout << "App" << std::endl;
 
+		if (prompt<std::string>("\nRepeat? (0 to exit): ") == "0") {
+			break;
+		}
+
+		std::cout << "\n- - - - - - - - - -\n\n";
+	}
 
 	return 0;
+}
+
+void printHello() {
+	std::cout << "* * * Algorithms: Dijkstra's algorithm * * *\n\n";
 }
 
 template <typename T>
