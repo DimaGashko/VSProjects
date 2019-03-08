c1ce19c17616b7b23f802d9f8b554e6e6a49ff09
DimaGashko <dgashko@mail.ua>
2019-03-08 20:35:05 +0200

    TreeTest

--- semester4-algorithms-labwork5/semester4-algorithms-labwork5/main.cpp
+++ semester4-algorithms-labwork5/semester4-algorithms-labwork5/main.cpp
@@ -12,21 +12,31 @@ int main() {
 	dg::Tree tree;
 
 	auto root = tree.getRoot();
-	dg::Tree::Node v1, v2, v3, v4, v5, v6, v7, v8;
+	dg::Tree::Node *v1, *v2, *v3, *v4, *v5, *v6, *v7, *v8;
 
-	v1 = dg::Tree::Node(2);
-	v2 = dg::Tree::Node(4);
-	v3 = dg::Tree::Node(6);
-	v4 = dg::Tree::Node(8);
-	v5 = dg::Tree::Node(10);
-	v6 = dg::Tree::Node(12);
-	v7 = dg::Tree::Node(14);
-	v8 = dg::Tree::Node(16);
+	v1 = new dg::Tree::Node(2);
+	v2 = new dg::Tree::Node(4);
+	v3 = new dg::Tree::Node(6);
+	v4 = new dg::Tree::Node(8);
+	v5 = new dg::Tree::Node(10);
+	v6 = new dg::Tree::Node(12);
+	v7 = new dg::Tree::Node(14);
+	v8 = new dg::Tree::Node(16);
 
-	root.value = 2;
+	root.value = 99;
 
-	root.left
+	root.left = v2;
+	root.right = v5;
 
+	v2->left = v3;
+	v2->right = v8;
+
+	v5->left = v6;
+	v5->right = v7;
+
+	v7->left = v1;
+	v7->right = v4;
+	system("pause");
 	return 0;
 }
 
