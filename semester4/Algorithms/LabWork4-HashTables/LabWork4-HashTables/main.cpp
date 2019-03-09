6c0327c11e240181b27c4f45deb2ade530734b53
DimaGashko <dgashko@mail.ua>
2019-03-09 15:11:51 +0200

    baseReadWordsFromFile

--- semester4/Algorithms/LabWork4-HashTables/LabWork4-HashTables/main.cpp
+++ semester4/Algorithms/LabWork4-HashTables/LabWork4-HashTables/main.cpp
@@ -4,8 +4,56 @@
 #include <string>
 #include <map>
 
-int main() {
-	
+void readWordsFromFile(std::vector<std::string> &words, const std::string url);
 
+template <typename T>
+T prompt(const char label[]);
+
+int main() {
+	std::vector<std::string> words(2000);
+	readWordsFromFile(words, "loremText2000.txt");
+
+	std::cout << words.size() << std::endl;
+
+	system("pause");
 	return  0;
+}
+
+void readWordsFromFile(std::vector<std::string>& words, const std::string url) {
+	std::ifstream fin(url);
+
+	if (!fin) {
+		throw std::runtime_error("Can't open the file");
+	}
+
+	std::string nextWord;
+
+	for (auto& word : words) {
+		if (!(fin >> nextWord)) break;
+		word = nextWord;
+	}
+
+	fin.close();
+}
+
+template <typename T>
+T prompt(const char label[]) {
+	std::cout << label;
+
+	while (true) {
+		T val;
+		std::cin >> val;
+
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			std::cout << "Wrong. Try again: ";
+		}
+		else {
+			std::cin.ignore(32767, '\n');
+			return val;
+		}
+
+	}
+
 }
