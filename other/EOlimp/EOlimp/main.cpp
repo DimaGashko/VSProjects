9cf250d901aa2d27183b58c8a3c35f2e672208d5
DimaGashko <dgashko@mail.ua>
2019-03-10 14:01:05 +0200

    base

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -4,30 +4,58 @@
 using namespace std;
 
 int n;
-vector<pair<int, vector<vector<int>>>> res;
+vector<vector<vector<int>>> res;
 vector<int> currentTasks;
 
 void init() {
 	cin >> n;
 
-	res = vector<pair<int, vector<vector<int>>>>(n);
+	res = vector<vector<vector<int>>>(n);
 }
 
-void next(vector<vector<int>> arr, int m, int i) {
+void next(vector<vector<int>> &arr, int m, int i = -1) {
 	currentTasks.push_back(i);
 
+	if (m == -1) {
+		vector<int> tasks(currentTasks.begin() + 1, currentTasks.end());
+		arr.push_back(tasks);
 
+		currentTasks.pop_back();
+		return;
+	}
+
+	for (int ni = i + 1; ni + m < n; ni++) {
+		next(arr, m - 1, ni);
+	}
+
+	currentTasks.pop_back();
 }
 
 void run() {
 	for (int m = 0; m < n; m++) {
-		next(res[m].second, m, -1);
+		next(res[m], m);
 	}
+}
+
+void printResult() {
+	for (int m = 0; m < n; m++) {
+
+		for (auto& tasks : res[m]) {
+			cout << m << " ";
+
+			for (auto& task : tasks) {
+				cout << task << " ";
+			}
+		}
+	}
+
+	cout << endl;
 }
 
 int main() {
 	init();
 	run();
+	printResult();
 		
 	system("pause");
 	return 0;
