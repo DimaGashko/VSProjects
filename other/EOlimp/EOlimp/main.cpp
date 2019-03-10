bd022a8fec6d44efb8692975fdf2777b8a3489e4
DimaGashko <dgashko@mail.ua>
2019-03-10 13:41:02 +0200

    init

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -3,10 +3,32 @@
 
 using namespace std;
 
+int n;
+vector<pair<int, vector<vector<int>>>> res;
+vector<int> currentTasks;
+
+void init() {
+	cin >> n;
+
+	res = vector<pair<int, vector<vector<int>>>>(n);
+}
+
+void next(vector<vector<int>> arr, int m, int i) {
+	currentTasks.push_back(i);
+
+
+}
+
+void run() {
+	for (int m = 0; m < n; m++) {
+		next(res[m].second, m, -1);
+	}
+}
+
 int main() {
-	
-
-	
-
+	init();
+	run();
+		
+	system("pause");
 	return 0;
 }
