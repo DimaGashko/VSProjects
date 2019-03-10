cd9769eaacc968a056b80054900c2afe44a55cbc
DimaGashko <dgashko@mail.ua>
2019-03-10 17:54:32 +0200

    A

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,65 +1,18 @@
 ﻿#include <iostream>
-#include <vector>
-#include <ctime>
 
 using namespace std;
 
-int n;
-vector<vector<vector<int>>> res;
-vector<int> currentTasks;
-
-void init() {
-	cin >> n;
-
-	res = vector<vector<vector<int>>>(n);
-}
-
-void next(vector<vector<int>> &arr, int m, int i = -1) {
-	currentTasks.push_back(i);
-
-	if (m == -1) {
-		vector<int> tasks(currentTasks.begin() + 1, currentTasks.end());
-		arr.push_back(tasks);
-
-		currentTasks.pop_back();
-		return;
-	}
-
-	for (int ni = i + 1; ni + m < n; ni++) {
-		next(arr, m - 1, ni);
-	}
-
-	currentTasks.pop_back();
-}
-
-void run() {
-	for (int m = 0; m < n; m++) {
-		next(res[m], m);
-	}
-}
-
-void printResult() {
-	for (int m = 0; m < n; m++) {
-
-		for (auto& tasks : res[m]) {
-			cout << (m + 1) << " ";
-
-			for (auto& task : tasks) {
-				cout << (task + 1) << " ";
-			}
-
-			cout << endl;
-		}
-	}
-}
-
 int main() {
-	init();
-	auto start = clock();
-	run();
-	cout << clock() - start << endl;
-	//printResult();
-		
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int p, d1, d2, d3;
+		cin >> p >> d1 >> d2 >> d3;
+
+		printf("%.2f\n", ((d1 * 5 + d2 * 3 + d3) / (double)p));
+	}
+
 	system("pause");
 	return 0;
 }
