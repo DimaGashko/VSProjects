7a917ba70396d2c6808ba567fd92ed028407c89f
DimaGashko <dgashko@mail.ua>
2019-03-10 21:36:08 +0200

    8634

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,19 +1,51 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
 
-int func(int n) {
-	if (n = 0) return 0;
-	return func(n - 1) + n;
+int getMulOfDigits(int n) {
+	int res = 1;
+
+	while (n) {
+		res *= n % 10;
+		n /= 10;
+	}
+
+	return res;
 }
 
 int main() {
-	int n = 0;
-	cin >> n;
+	int s, n; 
+	cin >> s >> n;
 
-	cout << func(n) << endl;
+	int k = 5000;
+
+	int* f = new int[k];
+	for (int i = 0; i < k; i++) {
+		f[i] = 0;
+	}
+
+	for (int i = s; i <= n; i++) {
+		f[getMulOfDigits(i)]++;
+	}
+
+	int common = 0;
+	int count = 0;
+
+	for (int i = 1; i < k; i++) {
+		if (!f[i]) continue;
+
+		if (f[i] > count) {
+			common = i;
+			count = f[i];
+		}
+	}
+
+	if (common == 0) {
+		cout << 0 << endl;
+	}
+	else {
+		cout << common << " " << count << endl;
+	}
 
 	system("pause");
 	return 0;
