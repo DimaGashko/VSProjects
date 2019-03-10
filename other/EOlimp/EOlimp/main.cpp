2d73eecc9399d6fa4901ef14b812c00d27c9ed3e
DimaGashko <dgashko@mail.ua>
2019-03-10 21:54:01 +0200

    done

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,51 +1,22 @@
 ﻿#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int getMulOfDigits(int n) {
-	int res = 1;
-
-	while (n) {
-		res *= n % 10;
-		n /= 10;
-	}
-
-	return res;
-}
-
 int main() {
-	int s, n; 
-	cin >> s >> n;
+	int n;
+	cin >> n;
 
-	int k = 5000;
+	int res;
 
-	int* f = new int[k];
-	for (int i = 0; i < k; i++) {
-		f[i] = 0;
-	}
-
-	for (int i = s; i <= n; i++) {
-		f[getMulOfDigits(i)]++;
-	}
-
-	int common = 0;
-	int count = 0;
-
-	for (int i = 1; i < k; i++) {
-		if (!f[i]) continue;
-
-		if (f[i] > count) {
-			common = i;
-			count = f[i];
-		}
-	}
-
-	if (common == 0) {
-		cout << 0 << endl;
+	if (n % 2 == 0) {
+		res = 1 + ((n - 2) / 2) * 3;
 	}
 	else {
-		cout << common << " " << count << endl;
+		res = 1 + ((n - 3) / 2) * 3 + 2;
 	}
+
+	cout << res << endl;
 
 	system("pause");
 	return 0;
