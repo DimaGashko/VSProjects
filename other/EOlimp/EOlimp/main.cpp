187e5a189cbaa05a599f3f8001da5f2b5ed98a06
DimaGashko <dgashko@mail.ua>
2019-03-10 22:52:32 +0200

    edit

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,22 +1,70 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+int swapCount = 0;
+string swaps = "";
+
+void counting_sort(vector<int>& elems, int min, int max) {
+	vector<unsigned> counts(max - min + 1);
+
+	for (int elem : elems)	{
+		++counts[elem - min];
+	}
+
+	auto elemsIt = elems.begin(); //current position to write result
+	for (int i = min; i <= max; ++i) {
+		// write counts[i - min] copies of i into elems
+		fill_n(elemsIt, counts[i - min], i);
+		elemsIt += counts[i - min];
+	}
+}
+
+void quick_sort(vector<int> &arr, int l, int r) {
+	int i = l, j = r;
+	int pivot = arr[(i + j) / 2];
+
+	while (i < j) {
+		while (arr[i] < pivot) i++;
+		while (arr[j] > pivot) j--;
+		if (i > j) break;
+
+		if (arr[i] != arr[j]) {
+			swapCount++;
+			//swaps += to_string(i) + " " + to_string(j) + "\n";
+
+			int arrI = arr[i];
+			arr[i] = arr[j];
+			arr[j] = arrI;
+		}
+
+		i++; j--;
+	}
+
+	if (i < r) quick_sort(arr, i, r);
+	if (j > l) quick_sort(arr, l, j);
+}
 
 int main() {
 	int n;
 	cin >> n;
 
-	int res;
+	vector<int> arr(1000000);
 
-	if (n % 2 == 0) {
-		res = 1 + ((n - 2) / 2) * 3;
-	}
-	else {
-		res = 1 + ((n - 3) / 2) * 3 + 2;
+	for (auto& item : arr) {
+		//cin >> item;
+		item = rand() % 1000;
 	}
 
-	cout << res << endl;
+	counting_sort(arr, 0, arr.size());
+
+	/*for (auto& item : arr) {
+		cout << item << " ";
+	}*/
+
+	//cout << swapCount << endl << swaps << endl;
 
 	system("pause");
 	return 0;
