52f43f74e820cb7e729162c0269719c01dcbddb7
DimaGashko <dgashko@mail.ua>
2019-03-12 20:25:38 +0200

    usefulSolution

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -7,27 +7,39 @@ using namespace std;
 typedef long long num;
 
 num getMinOfMatches(num n) {
-	int a = (int)cbrt(n);
+	num a = (num)cbrt(n);
 	n -= a * a * a;
 
 	num res = ((a + 1) * (a + 1) * a * 3);
 	if (n == 0) return res;
 
-	int fullSquares = n / (a * a);
+	num fullSquares = min(n / (a * a), (num)2);
+	res += (3 * a * a + 4 * a + 1) * fullSquares;
+	n -= a * a * fullSquares;
+	if (n == 0) return res;
+
+	if (fullSquares > 1) {
+		num p = min(n, a);
+		res += (p - 1) * 3 + 5;
+		n -= p;
+		if (n == 0) return res;
+	}
+
+	fullSquares = min(n / (a * a), (num)1);
 	res += (3 * a * a + 4 * a + 1) * fullSquares;
 	n -= a * a * fullSquares;
 	if (n == 0) return res;
 
 	if (fullSquares < 3) {
-		int b = (int)sqrt(n);
+		num b = (num)sqrt(n);
 		n -= b * b;
 		res += 3 * b * b + 4 * b + 1;
 
-		int twos = (n - 1) / b + 1;
+		num twos = (n - 1) / b + 1;
 		res += (n - twos) * 3 + twos * 5;
 	}
 	else {
-		int twos = (n - 1) / a + 1;
+		num twos = (n - 1) / a + 1;
 		res += (n - twos) * 3 + twos * 5;
 	}
 
