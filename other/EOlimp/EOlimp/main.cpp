21a02f865d205d42726255cc56806f331d365cf4
DimaGashko <dgashko@mail.ua>
2019-03-12 03:10:15 +0200

    base

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -4,50 +4,51 @@
 
 using namespace std;
 
-int getMinOfMatches(int n) {
-	int a = cbrt(n);
+typedef long long num;
+
+num getMinOfMatches(num n) {
+	num a = (num)cbrt(n);
 	n -= a * a * a;
 
-	int res = 0;
-
-	int twos, p;
+	num res = ((a + 1) * (a + 1) * a * 3) + (n * 12);
+	num twos, p;
 
 	// Stage 1
+	if (n == 0) return res;
 	p = min(n, 2 * a + 1);
 	twos = (p - 1 < a) ? 1 : 2;
 	res -= (p - twos) * 7 + twos * 4;
 	n -= p;
 
 	// Stage 2
-	if (n > 0) {
-		p = min(n, (2 * a + 1) * (a - 1));
-		twos = (p - 1) / (2 * a + 1) * 2 + ((p - 1) % (2 * a + 1) < a ? 1 : 2);
-		res -= (p - twos) * 9 + twos * 7;
-		n -= p;
-	}
+	if (n == 0) return res;
+	p = min(n, (2 * a + 1) * (a - 1));
+	twos = (p - 1) / (2 * a + 1) * 2 + ((p - 1) % (2 * a + 1) < a ? 1 : 2);
+	res -= (p - twos) * 9 + twos * 7;
+	n -= p;
 
 	// Stage 3
-	if (n > 0) {
-		p = min(n, a + 1);
-		res -= (p - 1) * 7 + 4;
-		n -= p;
-	}
+	if (n == 0) return res;
+	p = min(n, a + 1);
+	res -= (p - 1) * 7 + 4;
+	n -= p;
 
 	// Stage 4
-	if (n > 0) {
-		p = n;
-		twos = (p - 1) / (a + 1) + 1;
-		res -= (p - twos) * 9 + twos * 7;
-	}
+	if (n == 0) return res;
+	p = n;
+	twos = (p - 1) / (a + 1) + 1;
+	res -= (p - twos) * 9 + twos * 7;
 
 	return res;
 }
 
 int main() {
-	int n;
-	cin >> n;
+	while (true) {
+		num n;
+		cin >> n;
+		cout << getMinOfMatches(n) << endl;
+	}
 
-	cout << getMinOfMatches(n) << endl;
-
+	system("pause");
 	return 0;
 }
