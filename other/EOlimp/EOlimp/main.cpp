c4038e8f35c0fc4d2ab96390c0b522f62423bd8d
DimaGashko <dgashko@mail.ua>
2019-03-12 19:01:28 +0200

    4Tetst

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -7,28 +7,28 @@ using namespace std;
 typedef long long num;
 
 num getMinOfMatches(num n) {
-	num a = (num)cbrt(n);
+	int a = (int)cbrt(n);
 	n -= a * a * a;
 
-	num res = ((a + 1) * (a + 1) * a * 3) + (n * 12);
+	num res = ((a + 1) * (a + 1) * a * 3);
 	if (n == 0) return res;
 
 	int fullSquares = n / (a * a);
-	res -= ((a + 1) * (a + 1) * 2 * a) * fullSquares;
+	res += (3 * a * a + 4 * a + 1) * fullSquares;
 	n -= a * a * fullSquares;
 	if (n == 0) return res;
 
 	if (fullSquares < 3) {
-		num b = (num)sqrt(n);
+		int b = (int)sqrt(n);
 		n -= b * b;
-		res -= ((b + 1) * (b + 1) * 2 * b) + (n * 12);
+		res += 3 * b * b + 4 * b + 1;
 
 		int twos = (n - 1) / b + 1;
-		res -= (n - twos) * 9 + twos * 7;
+		res += (n - twos) * 3 + twos * 5;
 	}
 	else {
 		int twos = (n - 1) / a + 1;
-		res -= (n - twos) * 9 + twos * 7;
+		res += (n - twos) * 3 + twos * 5;
 	}
 
 	return res;
