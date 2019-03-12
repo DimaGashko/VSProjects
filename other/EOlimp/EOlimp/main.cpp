8eda938a96b7f97ce437e21b4edc3e958122424e
DimaGashko <dgashko@mail.ua>
2019-03-12 23:30:02 +0200

    optimizeGetDivs

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -7,14 +7,11 @@
 using namespace std;
 
 vector<int> getDivs(int n) {
-	vector<int> divs;
-	if (n == 0) return divs;
+	if (n == 0) return vector<int> {};
+	else if (n == 1) return vector<int> {1};
+	else if (n < 0) n = -n;
 
-	divs.push_back(1);
-	if (n == 1) return divs;
-
-	divs.push_back(n);
-	if (n == 2) return divs;
+	vector<int> divs{ 1, n };
 
 	for (int i = 2; i <= sqrt(n); i++) {
 		if (n % i != 0) continue;
