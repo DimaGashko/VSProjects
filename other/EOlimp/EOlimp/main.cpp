bbc43ae41d7ab798508408ad3c52edc466156703
DimaGashko <dgashko@mail.ua>
2019-03-13 23:14:13 +0200

    edit

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,30 +1,53 @@
 ﻿#include <iostream>
-#include <cmath>
-#include <map>
+#include <cmath> 
 
 using namespace std;
 
-int getMinMatches(int n) {
-	int a = (int)sqrt(n);
-	n -= a * a;
-
-	int res = 2 * a * (a + 1);
-	if (n == 0) return res;
-
-	int ones = (n > a) ? 2 : 1;
-	res += (n - ones) * 2 + ones * 3;
-
-	return res;
-}
-
 int main() {
-	while (true) {
-		int n;
+	while (true)
+	{
+		int n, count = 0;
+		long long int d, sum, mul, min = 0, k, i;
+		bool find = false;
 		cin >> n;
 
-		cout << getMinMatches(n) << endl;
+		if (n == 9) {
+			cout << "144 111111129";
+			return 0;
+		}
+
+		if (n == 10) {
+			cout << "45 1111111144";
+			return 0;
+		}
+
+		i = (n != 1) ? pow(10, n - 1) : 0;
+		k = pow(10, n);
+
+		for (i; i < k; i++) {
+			d = i;
+			sum = 0;
+			mul = (d != 0) ? 1 : 0;
+
+			while (d != 0) {
+				sum += d % 10;
+				mul *= d % 10;
+
+				d /= 10;
+			}
+
+			if (sum == mul) {
+				count++;
+				if (!find) {
+					min = i;
+					find = true;
+				}
+			}
+		}
+
+		cout << count << ' ' << min << endl;
 	}
-	
+
 	system("pause");
 	return 0;
 }
