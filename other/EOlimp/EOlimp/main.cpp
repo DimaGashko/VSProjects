0619f7e2ceaeae650c39f24b2c7cf01eb0a2bf36
DimaGashko <dgashko@mail.ua>
2019-03-14 00:41:53 +0200

    1612

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,52 +1,17 @@
 ﻿#include <iostream>
-#include <cmath> 
 
 using namespace std;
 
+
 int main() {
-	while (true)
-	{
-		int n, count = 0;
-		long long int d, sum, mul, min = 0, k, i;
-		bool find = false;
-		cin >> n;
+	int n;
+	cin >> n;
 
-		if (n == 9) {
-			cout << "144 111111129";
-			return 0;
-		}
+	int res = n;
 
-		if (n == 10) {
-			cout << "45 1111111144";
-			return 0;
-		}
+	for (int i = 1; (res = (res >> i) << i) == n; i++) {}
 
-		i = (n != 1) ? pow(10, n - 1) : 0;
-		k = pow(10, n);
-
-		for (i; i < k; i++) {
-			d = i;
-			sum = 0;
-			mul = (d != 0) ? 1 : 0;
-
-			while (d != 0) {
-				sum += d % 10;
-				mul *= d % 10;
-
-				d /= 10;
-			}
-
-			if (sum == mul) {
-				count++;
-				if (!find) {
-					min = i;
-					find = true;
-				}
-			}
-		}
-
-		cout << count << ' ' << min << endl;
-	}
+	cout << res << endl;
 
 	system("pause");
 	return 0;
