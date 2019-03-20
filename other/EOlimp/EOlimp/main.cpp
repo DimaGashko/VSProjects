e2ca32a138f3d1789c74a63aa6b1415d15852cca
DimaGashko <dgashko@mail.ua>
2019-03-20 23:29:17 +0200

    base

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,93 +1,38 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <vector>
 #include <string>
-#include <map>
+#include <cmath>
 
-std::map<char, char> baseAssociationMap{
-	{'a', '2'}, {'b', '2'}, {'c', '2'},
-	{'d', '3'}, {'e', '3'}, {'f', '3'},
-	{'g', '4'}, {'h', '4'}, {'i', '4'},
-	{'j', '5'}, {'k', '5'}, {'l', '5'},
-	{'m', '6'}, {'n', '6'}, {'o', '6'},
-	{'p', '7'}, {'r', '7'}, {'s', '7'},
-	{'t', '8'}, {'u', '8'}, {'v', '8'},
-	{'w', '9'}, {'x', '9'}, {'y', '9'}
+using namespace std;
+
+struct Time {
+	int h;
+	int m;
 };
 
-std::string toStandardForm(std::string number, std::map<char, char> associationMap);
-void printFrequency(std::map<std::string, int> & frequencyMap);
-
-void runTest();
+int getClockHandsAngle(Time time);
+Time parseTime(std::string strTime);
 
 int main() {
-	int numberOfTests;
-	std::cin >> numberOfTests;
+	
 
-	for (int testIndex = 0; testIndex < numberOfTests; testIndex++) {
-		runTest();
+	while (true) {
 
-		if (testIndex != numberOfTests - 1) {
-			std::cout << std::endl;
-		}
+
 	}
 
 	system("pause");
 	return 0;
 }
 
-void runTest() {
-	int numsCount;
-	std::cin >> numsCount;
+int getClockHandsAngle(Time time) {
+	// hAngle & mAndle are always int if h & m are int
+	// That's why result angle is int too
+	int hAngle = 360 / 12 * (time.h % 12);
+	int mAngle = 360 / 60 * (time.m % 60);
 
-	std::map<std::string, int> frequencyOfWords;
-
-	for (int i = 0; i < numsCount; i++) {
-		std::string number;
-		std::cin >> number;
-
-		std::string standardNum = toStandardForm(number, baseAssociationMap);
-		frequencyOfWords[standardNum]++;
-	}
-
-	printFrequency(frequencyOfWords);
+	int angle = abs(hAngle - mAngle) % 180;
 }
 
-std::string toStandardForm(std::string number, std::map<char, char> associationMap) {
-	std::string resNum = number;
+Time parseTime(std::string strTime) {
 
-	resNum.erase(
-		std::remove_if(resNum.begin(), resNum.end(), [](char c) {return c == '-'; }),
-		resNum.end()
-	);
-
-	std::transform(resNum.begin(), resNum.end(), resNum.begin(), ::tolower);
-	resNum.insert(resNum.begin() + 3, '-');
-
-	for (auto& c : resNum) {
-		if (!associationMap[c]) continue;
-		c = associationMap[c];
-	}
-
-	return resNum;
-}
-
-void printFrequency(std::map<std::string, int> &frequencyMap) {
-	std::vector<std::pair<std::string, int>> frequency(frequencyMap.begin(), frequencyMap.end());
-
-	frequency.erase(
-		std::remove_if(frequency.begin(), frequency.end(), [](std::pair<std::string, int> item) {
-		return item.second <= 1;
-	}), frequency.end());
-
-	if (frequency.empty()) {
-		std::cout << "No duplicates." << std::endl;
-		return;
-	}
-
-	std::sort(frequency.begin(), frequency.end());
-
-	for (auto& item : frequency) {
-		std::cout << item.first << " " << item.second << std::endl;
-	}
 }
