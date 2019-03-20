6858251de0b7de35b2a3fff2db2f7ea09c2a05f0
DimaGashko <dgashko@mail.ua>
2019-03-20 21:52:24 +0200

    baseNumbers

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,85 +1,44 @@
 ﻿#include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
+#include <map>
 
-void fillMinesMap(std::vector<std::string>& minesMap);
-void incrementMapCell(std::vector<std::vector<int>>& map, int i, int j);
+std::map<char, char> baseAssociationMap{
+	{'a', '2'}, {'b', '2'}, {'c', '2'},
+	{'d', '3'}, {'e', '3'}, {'f', '3'},
+	{'g', '4'}, {'h', '4'}, {'i', '4'},
+	{'j', '5'}, {'k', '5'}, {'l', '5'},
+	{'m', '6'}, {'n', '6'}, {'o', '6'},
+	{'p', '7'}, {'r', '7'}, {'s', '7'},
+	{'t', '8'}, {'u', '8'}, {'v', '8'},
+	{'w', '9'}, {'x', '9'}, {'y', '9'}
+};
 
-void writeMinesMap(std::vector<std::string>& minesMap, int n, int m);
-void printMinesMap(std::vector<std::string> &minesMap);
+std::string toStandardForm(std::string number, std::map<char, char> associationMap);
 
 int main() {
-	for (int fieldNumber = 1; true; fieldNumber++) {
-		int n, m;
+	std::cout << toStandardForm("TUT-GLOP", baseAssociationMap) << std::endl;
 
-		std::cin >> n >> m;
-		if (n == 0 && m == 0) break;
-
-		std::vector<std::string> minesMap;
-
-		writeMinesMap(minesMap, n, m);
-		fillMinesMap(minesMap);
-		
-		if (fieldNumber != 1) std::cout << std::endl;
-		std::cout << "Field #" << fieldNumber << ":" << std::endl;
-		printMinesMap(minesMap);
-	}
-
-	//system("pause");
+	system("pause");
 	return 0;
 }
 
-void fillMinesMap(std::vector<std::string>& minesMap) {
-	if (minesMap.empty()) return;
-	int iSize = minesMap.size();
-	int jSize = minesMap[0].size();
+std::string toStandardForm(std::string number, std::map<char, char> associationMap) {
+	std::string resNum = number;
 
-	std::vector<std::vector<int>> digitsMap(iSize, std::vector<int>(jSize));
+	resNum.erase(
+		std::remove_if(resNum.begin(), resNum.end(), [](char c) {return c == '-'; }),
+		resNum.end()
+	);
 
-	for (int i = 0; i < iSize; i++) {
-		for (int j = 0; j < jSize; j++) {
-			if (minesMap[i][j] != '*') continue;
+	std::transform(resNum.begin(), resNum.end(), resNum.begin(), ::tolower);
+	resNum.insert(resNum.begin() + 3, '-');
 
-			incrementMapCell(digitsMap, i + 1, j - 1);
-			incrementMapCell(digitsMap, i + 1, j + 1);
-			incrementMapCell(digitsMap, i - 1, j + 1);
-			incrementMapCell(digitsMap, i - 1, j - 1);
-			incrementMapCell(digitsMap, i + 1, j);
-			incrementMapCell(digitsMap, i - 1, j);
-			incrementMapCell(digitsMap, i, j + 1);
-			incrementMapCell(digitsMap, i, j - 1);
-		}
+	for (auto& c : resNum) {
+		if (!associationMap[c]) continue;
+		c = associationMap[c];
 	}
 
-	for (int i = 0; i < iSize; i++) {
-		for (int j = 0; j < jSize; j++) {
-			if (minesMap[i][j] == '*') continue;
-
-			minesMap[i][j] = digitsMap[i][j] + '0';
-		}
-	}
-}
-
-void incrementMapCell(std::vector<std::vector<int>>& map, int i, int j) {
-	if (map.empty()) return;
-
-	if (i < 0 || j < 0 || i >= (int)map.size() || j >= (int)map[0].size()) {
-		return;
-	}
-
-	map[i][j]++;
-}
-
-void writeMinesMap(std::vector<std::string>& minesMap, int n, int m) {
-	minesMap = std::vector<std::string>(n);
-
-	for (int i = 0; i < n; i++) {
-		std::cin >> minesMap[i];
-	}
-}
-
-void printMinesMap(std::vector<std::string> &minesMap) {
-	for (auto& row : minesMap) {
-		std::cout << row << std::endl;
-	}
+	return resNum;
 }
