5ef887b0faeeeb46fdbdb0d9feb0f33f12251913
DimaGashko <dimagashko@gmail.com>
2019-04-06 22:11:33 +0300

    baseSort

--- semester4/Algorithms/heap_sort/heap_sort/main.cpp
+++ semester4/Algorithms/heap_sort/heap_sort/main.cpp
@@ -2,31 +2,43 @@
 #include <vector>
 #include <cmath>
 
-void heapify(std::vector<int>& arr);
+void heapSort(std::vector<int>& arr);
+void heapify(std::vector<int>& arr, int size, int index);
 void printArr(std::vector<int>& arr);
 
 int main() {
-	std::vector<int> arr{5,13,54,4,1,3,2};
+	std::vector<int> arr{5,13,54,4,1,3,60};
 
-	while (true) {
-		int a;
-		std::cin >> a;
-		std::cout << floor((a + 1) / 2) - 1 << std::endl;
-	}
+	heapSort(arr);
+
+	printArr(arr);
 
 	system("pause");
 	return 0;
 }
 
-void heapify(std::vector<int>& arr, int index) {
+void heapSort(std::vector<int>& arr) {
+	for (int i = arr.size() / 2 - 1; i >= 0; i--) {
+		heapify(arr, arr.size(), i);
+	}
+
+	for (int i = arr.size() - 1; i >= 0; i--) {
+		//std::swap(arr[0], arr[arr.size() - 1]);
+		heapify(arr, i, 0);
+	}
+}
+
+void heapify(std::vector<int>& arr, int size, int index) {
 	int l = index * 2 + 1;
 	int r = index * 2 + 2;
+	int max = index;
 
-	int max = (arr[l] > arr[r]) ? l : r;
-	if (arr[index] > arr[max]) return;
+	if (l < size && arr[l] > arr[max]) max = l;
+	if (r < size && arr[r] > arr[max]) max = r;
+	if (max == index) return;
 
 	std::swap(arr[index], arr[max]);
-	heapify(arr, max);
+	heapify(arr, size, max);
 }
 
 void printArr(std::vector<int>& arr) {
