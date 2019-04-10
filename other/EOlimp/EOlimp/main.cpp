a47fb3e9dca03dcd065bf537c8afae43808cbeee
DimaGashko <dimagashko@gmail.com>
2019-04-10 18:03:57 +0300

    edit

--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,25 +1,55 @@
 ﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <deque>
+#include <cmath>
+#include <map>
 
 using namespace std;
 
-// 1327 - реальная задача найти fact(1000)
+vector<int> arr;
+vector<int> res;
+map<int, int> numMap;
+int n, len = 0;
 
-long long fact(long long n) {
-	long long res = 1;
-
-	for (int i = 2; i <= n; i++) {
-		res *= i;
-	}
-
-	return res;
-}
-
-int main() {
-	long long n;
+void init() {
 	cin >> n;
 
-	cout << fact(n) << endl; 
+	arr = vector<int>(n);
 
-	system("pause");
+	for (auto& item : arr) {
+		cin >> item;
+		numMap[item]++;
+	}
+}
+
+
+int main() {
+	init();
+
+	for (int i = n - 1; i >= 0; i--) {
+		int item = arr[i];
+
+		if (item % 4 != 0) {
+			res.push_back(item);
+			continue;
+		}
+
+		int newv = item / 4;
+		
+		numMap[item]--;
+
+		if (numMap[newv] > 0) {
+			res.push_back(newv);
+			numMap[newv]--;
+		}
+	}
+
+	for (int i = res.size() - 1; i >= 0; i--) {
+		cout << res[i] << endl;
+	}
+
+	//system("pause");
 	return 0;
 }
