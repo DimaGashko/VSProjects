b012b817a57ad37fa9960e50c101a452d9fdc6b6
DimaGashko <dimagashko@gmail.com>
2019-04-12 03:26:43 +0300

    FridayDone

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -2,50 +2,82 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <ctime>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
 int n, resLen = 0;
-vector<int> arr;
-vector<int> res;
-map<int, int> numMap;
+int *arr;
+int *res;
+bool *visited;
 
 void init() {
     cin >> n;
 
-    arr = vector<int>(n);
-    res = vector<int>(n / 2);
+    arr = new int[n];
+    res = new int[n / 2];
+    visited = new bool[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = 0;
+        visited[i] = false;
+    }
+
+    for (int i = 0; i < n / 2; i++) {
+        res[i] = 0;
+    }
 
     for (int i = n - 1; i >= 0; i--) {
         cin >> arr[i];
     }
+}
 
-    for (auto &num : arr) {
-        numMap[num]++;
+int search(int l, int val) {
+    int r = n - 1;
+
+    while (r >= l) {
+        int m = (l + r) / 2;
+
+        if (arr[m] == val) {
+            while (arr[m] == val && !visited[m]) {m--;}
+            while (arr[m] != val || visited[m]) {m++;}
+
+            return (arr[m] == val && !visited[m]) ? m : -1;
+        }
+
+        else if (arr[m] > val) l = m + 1;
+        else if (arr[m] < val) r = m - 1;
     }
+
+    return -1;
 }
 
 void run() {
-    for (auto &cur : arr) {
-        if (numMap[cur] <= 0) continue;
+    for (int i = 0 ; i < n; i++) {
+        if (visited[i]) continue;
 
-        int newV = int(cur * 0.75);
-        if (numMap[newV] <= 0) continue;
+        int newV = int(arr[i] * 0.75);
+        int index = search(i, newV);
 
+        if (index == -1) continue;
         res[resLen++] = newV;
-        numMap[newV]--;
+        visited[index] = true;
     }
 
-    sort(res.begin(), res.end());
 }
 
 void printRes() {
-    for (auto &num : res) {
-        cout << num << "\n";
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        cout << res[i] << '\n';
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     init();
     run();
     printRes();
--- other/EOlimp/EOlimp/main.cpp
+++ other/EOlimp/EOlimp/main.cpp
@@ -1,55 +1,97 @@
 ﻿#include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
-#include <deque>
-#include <cmath>
 #include <map>
+#include <ctime>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
+int n, resLen = 0;
 vector<int> arr;
 vector<int> res;
 map<int, int> numMap;
-int n, len = 0;
+
+string correct;
+
+void fInit() {
+	srand((int)time(nullptr));
+
+	n = 5500;
+	arr = vector<int>(n);
+	res = vector<int>(n / 2);
+	vector<int> st(n / 2);
+	vector<int> nw(n / 2);
+
+	for (int i = 0; i < n / 2; i++) {
+		st[i] = 4 * int(((double)rand() / RAND_MAX) * 1000000000 / 4) + 1;
+		arr[i] = st[i];
+	}
+
+	for (int i = 0; i < n / 2; i++) {
+		nw[i] = int(st[i] * 0.75);
+		arr[n / 2 + i] = nw[i];
+	}
+
+	sort(st.begin(), st.end());
+	sort(nw.begin(), nw.end());
+	sort(arr.begin(), arr.end());
+
+	for (auto& a : nw) {
+		correct += to_string(a) + '\n';
+	}
+
+	for (auto& num : arr) {
+		numMap[num]++;
+	}
+}
 
 void init() {
 	cin >> n;
 
 	arr = vector<int>(n);
+	res = vector<int>(n / 2);
 
-	for (auto& item : arr) {
-		cin >> item;
-		numMap[item]++;
+	for (int i = n - 1; i >= 0; i--) {
+		cin >> arr[i];
+	}
+
+	for (auto& num : arr) {
+		numMap[num]++;
 	}
 }
 
+void run() {
+	for (auto& cur : arr) {
+		if (numMap[cur] <= 0) continue;
+
+		int newV = int(cur * 0.75);
+		if (numMap[newV] <= 0) continue;
+
+		res[resLen++] = newV;
+		numMap[newV]--;
+	}
+
+	sort(res.begin(), res.end());
+}
+
+void printRes() {
+	string r;
+
+	for (auto& num : res) {
+		r += to_string(num) + '\n';
+	}
+
+	cout << (r == correct) << endl << endl;
+
+	//cout << r << endl << endl << correct;
+}
 
 int main() {
-	init();
+	fInit();
+	run();
+	printRes();
 
-	for (int i = n - 1; i >= 0; i--) {
-		int item = arr[i];
-
-		if (item % 4 != 0) {
-			res.push_back(item);
-			continue;
-		}
-
-		int newv = item / 4;
-		
-		numMap[item]--;
-
-		if (numMap[newv] > 0) {
-			res.push_back(newv);
-			numMap[newv]--;
-		}
-	}
-
-	for (int i = res.size() - 1; i >= 0; i--) {
-		cout << res[i] << endl;
-	}
-
-	//system("pause");
 	return 0;
 }
