5102f3be372ab7b142f6331f9e7e37290d51468b
DimaGashko <dimagashko@gmail.com>
2019-04-12 17:02:45 +0300

    baseOf

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -1,57 +1,84 @@
+#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
+
+void getPrimesTo(vector<int> &res, int to) {
+    const int offset = 2;
+    double maxP = sqrt(to);
+
+    // Если использовать вектор getPrimesTo на 10^6 занимает ~280ms, с массивом - 40
+    bool *primes = new bool[to - offset];
+    for (int i = 0; i < to - offset; i++) {
+        primes[i] = true;
+    }
+    for (int p = 2; p <= maxP; p++) {
+        for (int i = 2 * p - offset; i <= to - offset; i += p) {
+            primes[i] = false;
+        }
+    }
+
+    for (int i = 0; i < to - offset; i++) {
+        // push_back почти не влияет на скорость, так как простых чисел максимум 80000
+        if (primes[i]) res.push_back(i + offset);
+    }
+}
+
+void getColumnSizes(vector<int> &nums, vector<int> &colSizes) {
+    int rSize = nums.size();
+
+    vector<int> rowSizes(rSize);
+    for (int i = 0; i < rSize; i++) {
+        rowSizes[i] = nums[i] / 2;
+    }
+
+    int cSize = rowSizes.back();
+    colSizes = vector<int>(cSize);
+
+    for (int i = 0; i < rSize; i++) {
+        int cur = rowSizes[i] - 1;
+
+        if (colSizes[cur] == 0) {
+            colSizes[cur] = rSize - i;
+        }
+
+        for (int prev = cur - 1; colSizes[prev] == 0; prev--) {
+            colSizes[prev] = colSizes[cur];
+        }
+    }
+
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
+    //long long k;
+    //cin >> k;
 
-    cin >> n;
-    vector<int> arr(n);
+    vector<int> primes, colSizes;
+    getPrimesTo(primes, 18);
+    getColumnSizes(primes, colSizes);
+for (int k = 0; k < 25; k++) {
+    int row = k;
+    int col = 1;
+    int i = 0;
 
-    for (auto &el : arr) {
-        cin >> el;
+    for (; i < colSizes.size() && row > colSizes[i]; i++) {
+        row -= colSizes[i];
+        col++;
     }
 
-    int a = arr[0], b = 0, c = 0;
-    int ai = 0, bi = 0, ci = 0;
-    int rai = 0, rci = 0;
+    if (row > colSizes.back() && 0) {
+        cout << "-1 -1" << "\n";
 
-    for (int i = 1; i <= arr.size() - 1; i++) {
-        if (arr[i] < b && b != 0) {
-            c = arr[i];
-            ci = i;
+    } else {
+        int prime = primes[row + i - 1];
+        int a = col;
+        int b = prime - col;
 
-            if (rci - rai == 0 || ci - ai < rci - rai) {
-                rai = ai;
-                rci = ci;
-            }
-
-            a = b;
-            ai = bi;
-            i = ai;
-            b = 0;
-        }
-
-        else if (arr[i] <= a) {
-            a = arr[i];
-            ai = i;
-        }
-        else  {
-            if (b != 0 && arr[i] != b) {
-                a = b;
-                ai = bi;
-            }
-
-            b = arr[i];
-            bi = i;
-        }
+        cout << a << " " << b << "\n";
     }
-
-    if (rci - rai == 0) cout << 0 << endl;
-    else cout << rai + 1 << " " << rci + 1 << endl;
-
+}
     return 0;
 }
