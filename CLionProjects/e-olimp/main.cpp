f937b5cfe8b7f171761c7de766c3b927eba1efe6
DimaGashko <dimagashko@gmail.com>
2019-04-12 13:14:14 +0300

    E

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -3,11 +3,55 @@
 using namespace std;
 
 int main() {
-    int n, res = 0;
-    cin >> n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    for (; n != 0; n >>= 1) res++;
-    cout << res << endl;
+    int n;
+
+    cin >> n;
+    vector<int> arr(n);
+
+    for (auto &el : arr) {
+        cin >> el;
+    }
+
+    int a = arr[0], b = 0, c = 0;
+    int ai = 0, bi = 0, ci = 0;
+    int rai = 0, rci = 0;
+
+    for (int i = 1; i <= arr.size() - 1; i++) {
+        if (arr[i] < b && b != 0) {
+            c = arr[i];
+            ci = i;
+
+            if (rci - rai == 0 || ci - ai < rci - rai) {
+                rai = ai;
+                rci = ci;
+            }
+
+            a = b;
+            ai = bi;
+            i = ai;
+            b = 0;
+        }
+
+        else if (arr[i] <= a) {
+            a = arr[i];
+            ai = i;
+        }
+        else  {
+            if (b != 0 && arr[i] != b) {
+                a = b;
+                ai = bi;
+            }
+
+            b = arr[i];
+            bi = i;
+        }
+    }
+
+    if (rci - rai == 0) cout << 0 << endl;
+    else cout << rai + 1 << " " << rci + 1 << endl;
 
     return 0;
 }
