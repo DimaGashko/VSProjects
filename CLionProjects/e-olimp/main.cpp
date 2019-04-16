d534a4df2badad8b112a35b597ce8053ca5b39fe
DimaGashko <dimagashko@gmail.com>
2019-04-16 13:36:12 +0300

    H

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -1,60 +1,8 @@
 #include <bits/stdc++.h>
 
-typedef unsigned int uint;
-
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n, l = -1, r = -1, t = 0;
-    cin >> n;
-
-    vector<int> arr(2 * n);
-    vector<pair<int, int>> pairs;
-
-    for (auto &item : arr) {
-        cin >> item;
-    }
-
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i - 1] != arr[i]) continue;
-
-        if (l == -1) {
-            l = i;
-        }
-        else {
-            r = i - 1;
-            pairs.emplace_back(l, r);
-            t++;
-            l = -1;
-            r = -1;
-        }
-    }
-
-    if (r == -1 && l != -1) {
-        t++;
-        if (l < arr.size() - 1){
-            pairs.emplace_back(l, l + 1);
-        } else {
-            pairs.emplace_back(l - 3, l);
-        }
-
-    }
-
-    cout << t << "\n";
-
-    for (auto &item : pairs) {
-        cout << item.first + 1 << " " << item.second + 1 << "\n";
-    }
-
-    return 0;
-}
-
-/*
-  // выборы
-  int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -75,4 +23,4 @@ int main() {
 
     cout << "NO";
     return 0;
-}*/
+}
