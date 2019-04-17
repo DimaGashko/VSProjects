779d0cc297b32306fd0aff1b5ee59cd87d813faa
DimaGashko <dimagashko@gmail.com>
2019-04-18 00:30:08 +0300

    edit

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -4,23 +4,41 @@ using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie();
 
-    int n, k;
-    cin >> n >> k;
+    int n;
+    cin >> n;
 
-    std::vector<int> vec(n, 0);
-    for (int i = 0; i < n; ++i) {
-        int index;
-        cin >> index;
+    set<int> nums;
 
-        vec[index - 1]++;
-        if (vec[index - 1] > floor(n / 2.0)) {
-            cout << "YES";
-            return 0;
-        }
+    int tmp;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        nums.insert(tmp);
     }
 
-    cout << "NO";
+    int size = nums.size();
+
+    if (size > 3) {
+        cout << "-1\n";
+        return 0;
+    }
+
+    int res = -1;
+    vector<int> ns(nums.begin(), nums.end());
+
+    if (size == 2) {
+        int d = ns[1] - ns[0];
+
+        res = (d % 2 == 0) ? d / 2 : d;
+
+    } else if (size == 3) {
+        res = (ns[1] - ns[0] == ns[2] - ns[1]) ? ns[1] - ns[0] : -1;
+    } else if (size == 1) {
+        res = 0;
+    }
+
+    cout << (res != -1 ? abs(res) : res) << endl;
+
     return 0;
 }
