c01f920f821d0060f38217ff4e83f6033d049583
DimaGashko <dimagashko@gmail.com>
2019-04-20 00:08:36 +0300

    A

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -6,7 +6,35 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << "Hello" << endl;
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (auto &item : arr) {
+        cin >> item;
+    }
+
+    int max = 1, curSize = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[i - 1]) {
+            curSize++;
+            continue;
+        }
+
+        if (curSize > max) {
+            max = curSize;
+        }
+
+        curSize = 1;
+    }
+
+    if (curSize > max) {
+        max = curSize;
+    }
+
+    cout << max << "\n";
 
     return 0;
 }
