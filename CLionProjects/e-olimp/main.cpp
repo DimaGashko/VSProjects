a330d3c1ba6f8979daa0052adca4e891fddb4214
DimaGashko <dimagashko@gmail.com>
2019-04-20 23:19:46 +0300

    edit

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -4,22 +4,31 @@ using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie();
+    cin.tie(nullptr);
 
-    int n;
+    int n, res = 0;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        int next;
-        cin >> next;
+    vector<pair<int, int>> arr(n);
+    vector<bool> visited(n);
 
-        if (next == 1) {
-            cout << "HARD" << endl;
-            return 0;
-        }
+    for (auto &p : arr) {
+        cin >> p.first;
+        cin >>p.second;
     }
 
-    cout << "EASY" << endl;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (k == i || k == j) continue;
+
+                for (int p = 0; p < n; i++) {
+
+                }
+
+            }
+        }
+    }
 
     return 0;
 }
