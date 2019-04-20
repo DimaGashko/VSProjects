9805194f2c834d54d83889f689c694ce40b04cd4
DimaGashko <dimagashko@gmail.com>
2019-04-20 14:28:21 +0300

    edit

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -10,31 +11,49 @@ int main() {
     cin >> n;
 
     vector<int> arr(n);
+    vector<pair<int, int>> res;
 
     for (auto &item : arr) {
         cin >> item;
     }
 
-    int max = 1, curSize = 1;
+    unordered_map<int, int> numMap;
 
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > arr[i - 1]) {
-            curSize++;
-            continue;
-        }
-
-        if (curSize > max) {
-            max = curSize;
-        }
-
-        curSize = 1;
+    for (auto &item : arr) {
+        numMap[item] = numMap[item] + 1;
     }
 
-    if (curSize > max) {
-        max = curSize;
+    int common = -1;
+    int commonCount = -1;
+
+    for (auto &item : numMap) {
+        if (item.second <= commonCount) continue;
+
+        common = item.first;
+        commonCount = item.second;
     }
 
-    cout << max << "\n";
+    int firstCommon = 0;
+    while (arr[firstCommon] != common) firstCommon++;
+
+    cout << firstCommon << endl;
+
+   /* for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] == arr[i + 1]) continue;
+
+        int d = arr[i + 1] - arr[i];
+        int op = (d > 0) ? 1 : 2;
+
+        arr[i] += d;
+
+        res.emplace_back(op, i + 1);
+    }*/
+
+    cout << res.size() << "\n";
+
+    for (auto &item : res) {
+        cout << item.first << " " << item.second << " " << item.second + 1 << "\n";
+    }
 
     return 0;
 }
