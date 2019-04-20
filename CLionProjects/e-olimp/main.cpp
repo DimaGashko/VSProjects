dd2359c32d92289430633976c60d13ff391186ec
DimaGashko <dimagashko@gmail.com>
2019-04-20 20:59:16 +0300

    easyProblem

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -2,63 +2,24 @@
 
 using namespace std;
 
-
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie();
 
     int n;
     cin >> n;
 
-    vector<int> arr(n);
-    vector<array<int, 3>> res;
+    for (int i = 0; i < n; i++) {
+        int next;
+        cin >> next;
 
-    for (auto &item : arr) {
-        cin >> item;
+        if (next == 1) {
+            cout << "HARD" << endl;
+            return 0;
+        }
     }
 
-    unordered_map<int, int> numMap;
-
-    for (auto &item : arr) {
-        numMap[item] = numMap[item] + 1;
-    }
-
-    int common = -1;
-    int commonCount = -1;
-
-    for (auto &item : numMap) {
-        if (item.second <= commonCount) continue;
-
-        common = item.first;
-        commonCount = item.second;
-    }
-
-    int firstCommon = 0;
-    while (arr[firstCommon] != common) firstCommon++;
-
-    for (int i = firstCommon - 1; i >= 0; i--) {
-        if (arr[i] == common) continue;
-
-        int d = common - arr[i];
-        int op = (d > 0) ? 1 : 2;
-
-        res.push_back(array<int, 3>{{op, i + 1, i + 2}});
-    }
-
-    for (int i = firstCommon + 1; i < n; i++) {
-        if (arr[i] == common) continue;
-
-        int d = common - arr[i];
-        int op = (d > 0) ? 1 : 2;
-
-        res.push_back(array<int, 3>{{op, i + 1, i}});
-    }
-
-    cout << res.size() << "\n";
-
-    for (auto &item : res) {
-        cout << item[0] << " " << item[1] << " " << item[2] << "\n";
-    }
+    cout << "EASY" << endl;
 
     return 0;
 }
