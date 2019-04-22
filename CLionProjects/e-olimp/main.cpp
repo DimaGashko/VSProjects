2ab842e90f768e3396a476f69f12c4b99885e88d
DimaGashko <dimagashko@gmail.com>
2019-04-22 23:11:21 +0300

    https://codeforces.com/problemset/problem/959/A

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -8,17 +8,22 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int pos = 0, a;
+    int n;
+    string s;
+    cin >> n >> s;
 
-    for (;;pos++) {
-        cin >> a;
-        if (a == 1) break;
+    int sf = 0;
+    int fs = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1]) continue;
+
+        if (s[i] == 'F') sf++;
+        else fs++;
     }
 
-    int x = pos % 5;
-    int y = pos / 5;
-
-    cout << abs(x - 2) + abs(y - 2) << endl;
+    cout << (sf > fs ? "YES" : "NO") << endl;
 
     return 0;
 }
+
