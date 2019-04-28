3eadf69bc950291d2ad2cad1974bd6aaf879f45d
DimaGashko <dimagashko@gmail.com>
2019-04-28 19:27:08 +0300

    edit

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -4,35 +4,8 @@ using namespace std;
 
 #define endl "\n"
 
-int *arr;
-string res;
-string s;
-
-void run(char side, int l, int r, int prev) {
-    int cur = (side == 'L') ? arr[l] : arr[r];
-
-    if (cur > prev && l <= r) {
-        s.push_back(side);
-
-        int newL = (side == 'L') ? l + 1 : l;
-        int newR = (side == 'L') ? r : r - 1;
-
-        if ((arr[l + 1] < arr[r] && arr[l + 1] > arr[l]) || l + 1 > r) {
-            run('L', newL, newR, cur);
-        }
-
-        if ((arr[r] < arr[l + 1]  && arr[r] > arr[l + 1]) || l + 1 > r) {
-            run('R', newL, newR, cur);
-        }
-
-        s.pop_back();
-
-    }
-
-    if (s.length() > res.length()) {
-        res = string(s.begin(), s.end());
-    }
-
+int get(int s, int v, int t) {
+    return s * v + t * 2;
 }
 
 int main() {
@@ -43,16 +16,22 @@ int main() {
     int n;
     cin >> n;
 
-    arr = new int[n];
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    s.reserve(200000);
-    run('L', 0, n - 1, -1);
+    vector<int> arr(n);
+    for (int &a : arr) cin >> a;
 
-    if (arr[0] >= arr[n - 1]) {
-        run('R', 0, n - 1, -1);
+    std::map<int, int> map;
+
+    for (int a : arr) {
+        map[a] = map[a] + 1;
     }
 
-    cout << res.length() << endl << res << endl;
+    int max = 0;
+
+    for (auto item : map) {
+        if (item.second > max) max = item.second;
+    }
+
+    cout << max << endl;
 
     return 0;
 }
