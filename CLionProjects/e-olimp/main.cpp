6e0d4474a5fc2c9ffa3ab8fa84c54eceed4d6393
DimaGashko <dimagashko@gmail.com>
2019-05-10 15:43:17 +0300

    https://codeforces.com/problemset/problem/658/A

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -4,33 +4,44 @@ using namespace std;
 
 #define endl "\n"
 
+int n, c;
+
+int get(vector<int> &costs, vector<int> &times) {
+    int sum = 0, curTime = 0;
+
+    for (int i = 0; i < n; i++) {
+        curTime += times[i];
+        sum += max(costs[i] - c * curTime, 0);
+    }
+
+    return sum;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n, m, tmp;
-    cin >> n >> m;
+    cin >> n >> c;
 
-    vector<int> arr(n), map(10), res;
+    vector<int> costs(n), times(n);
 
-    for (int &a : arr) cin >> a;
-    for (int i = 0; i < m; i++) {
-        cin >> tmp;
-        map[tmp] = 1;
+    for (int &a : costs) cin >> a;
+    for (int &a : times) cin >> a;
+
+    int l = get(costs, times);
+
+    reverse(costs.begin(), costs.end());
+    reverse(times.begin(), times.end());
+
+    int r = get(costs, times);
+
+    if (l > r) {
+        cout << "Limak" << endl;
+    } else if (r > l) {
+        cout << "Radewoosh" << endl;
+    } else {
+        cout << "Tie";
     }
-
-    for (auto &a : arr) {
-        if (!map[a]) continue;
-
-        res.push_back(a);
-        map[a] = 0;
-    }
-
-    for (int &a : res) {
-        cout << a << " ";
-    }
-
-    cout << endl;
 
     return 0;
 }
