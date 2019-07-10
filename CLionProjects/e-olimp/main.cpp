a04ef0914112506293ed77df4756344c2c716d2c
DimaGashko <dmitry.gashko@postindustria.com>
2019-07-10 21:50:18 +0300

    https://codeforces.com/problemset/problem/1186/A

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -8,24 +8,14 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n, ones = 0;
-    string s;
+    int n, m, k;
+    cin >> n >> m >> k;
 
-    cin >> n >> s;
-
-    for (auto &c : s) {
-        if (c == '1') ones++;
-    }
-
-    if (n % 2 != 0 || ones != n / 2.0) {
-        cout << 1 << endl << s << endl;
+    if (m < n || k < n) {
+        cout << "no" << endl;
         return 0;
     }
 
-    string s2 = string(s.begin() + 1, s.end());
-
-    cout << 2 << endl;
-    cout << s[0] << ' ' << s2 << endl;
-
+    cout << "yes" << endl;
     return 0;
 }
