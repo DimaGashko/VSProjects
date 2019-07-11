2b199aec0ecd79c5e733c76398ec328550dae978
DimaGashko <dmitry.gashko@postindustria.com>
2019-07-11 22:52:10 +0300

    https://codeforces.com/problemset/problem/1173/A

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -4,18 +4,21 @@ using namespace std;
 
 #define endl '\n'
 
+char get(int x, int y, int z) {
+    if (x == y && z == 0) return '0';
+    if (x > y + z) return '+';
+    if (y > x + z) return '-';
+
+    return '?';
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n, m, k;
-    cin >> n >> m >> k;
+    int x, y, z;
+    cin >> x >> y >> z;
 
-    if (m < n || k < n) {
-        cout << "no" << endl;
-        return 0;
-    }
-
-    cout << "yes" << endl;
+    cout << get(x, y, z) << endl;
     return 0;
 }
