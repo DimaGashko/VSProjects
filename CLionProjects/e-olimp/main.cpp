d9f0bfc824d005c3001f4531f506f16a4dc19130
DimaGashko <dmitry.gashko@postindustria.com>
2019-08-29 21:45:26 +0300

    edit

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -2,21 +2,58 @@
 
 using namespace std;
 
-#define endl '\n'
+int run() {
+    int k, n, a, b;
+    cin >> k >> n >> a >> b;
+
+    if (b * n >= k) return -1;
+
+    int res = n - max((a * n - k + 1), 0) * 1.f / (a - b);
+    return res;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n;
-    cin >> n;
+    int q;
+    cin >> q;
 
-    for (int i = 0; i < n; i++) {
-        long long a, b, c;
-        cin >> a >> b >> c;
-
-        cout << (a + b + c) / 2 << endl;
+    for (int i = 0; i < q; i++) {
+        cout << run() << endl;
     }
 
     return 0;
 }
+
+/* A
+int getDigitsSum(int a) {
+    int sum = 0;
+
+    while (a) {
+        sum += a % 10;
+        a /= 10;
+    }
+
+    return sum;
+}
+
+bool isInterest(int a) {
+    return getDigitsSum(a) % 4 == 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int a;
+    cin >> a;
+
+    while (!isInterest(a)) {
+        a++;
+    }
+
+    cout << a;
+
+    return 0;
+}*/
