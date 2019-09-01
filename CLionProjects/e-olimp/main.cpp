0154f130fce157efb74f552816190eb424d9d391
DimaGashko <dmitry.gashko@postindustria.com>
2019-09-01 14:55:01 +0300

    edit

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -3,12 +3,61 @@
 using namespace std;
 
 int run() {
-    int k, n, a, b;
-    cin >> k >> n >> a >> b;
+    int n;
+    cin >> n;
 
-    if (b * n >= k) return -1;
+    vector<int> arr(n);
 
-    int res = n - max((a * n - k + 1), 0) * 1.f / (a - b);
+    for (int &a : arr) {
+        cin >> a;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << run() << endl;
+    }
+
+    return 0;
+}
+
+/*
+// B
+
+int run() {
+    int n, res = 0, minIndex = 0;
+    cin >> n;
+
+    vector<int> arr(n);
+    map<int, int> m;
+
+    for (int &a : arr) {
+        cin >> a;
+        m[a]++;
+    }
+
+    vector<int> sorted(arr.begin(), arr.end());
+    sort(sorted.begin(), sorted.end());
+
+    for (int i = 0; i < n - 1; i++) {
+        m[arr[i]]--;
+
+        while (m[sorted[minIndex]] < 1) {
+            minIndex++;
+        }
+
+        if (arr[i] > sorted[minIndex]) {
+            res++;
+        }
+
+    }
+
     return res;
 }
 
@@ -16,44 +65,50 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int q;
-    cin >> q;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < q; i++) {
+    for (int i = 0; i < n; i++) {
         cout << run() << endl;
     }
 
     return 0;
-}
+}*/
 
-/* A
-int getDigitsSum(int a) {
-    int sum = 0;
-
-    while (a) {
-        sum += a % 10;
-        a /= 10;
-    }
-
-    return sum;
-}
-
-bool isInterest(int a) {
-    return getDigitsSum(a) % 4 == 0;
-}
+/*
+// A
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int a;
-    cin >> a;
+    int n;
+    cin >> n;
 
-    while (!isInterest(a)) {
-        a++;
+    vector<int> arr(n);
+
+    for (int &a : arr) {
+        cin >> a;
     }
 
-    cout << a;
+    vector<int> res;
+
+    for (int &a : arr) {
+        int sum = 0;
+
+        for (int &b : arr) {
+            sum += ((b - a) % 2 != 0) ? 1 : 0;
+        }
+
+        res.push_back(sum);
+    }
+
+    sort(res.begin(), res.end());
+
+    cout << res[0] << endl;
 
     return 0;
-}*/
+}
+
+
+ */
