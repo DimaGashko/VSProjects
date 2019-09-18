9b50141f65dfd98e53dd8228de11e04f5f975e45
DimaGashko <dmitry.gashko@postindustria.com>
2019-09-18 22:52:01 +0300

    codeforces 586

--- CLionProjects/e-olimp/main.cpp
+++ CLionProjects/e-olimp/main.cpp
@@ -4,43 +4,69 @@ using namespace std;
 
 typedef long long ll;
 
-#define vi vector<int>
-#define mii map<int, int>
-
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    cin.tie();
+    cout.tie();
 
-    int n, res = 0, prev = -1, cur, realAi = -1;
-    cin >> n;
-    n *= 2;
+    string s;
+    cin >> s;
 
-    vi v(n);
-    mii m;
+    char min = s[0];
 
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-        m[v[i]] = i;
+    for (auto c : s) {
+        if (c > min) {
+            cout << "Ann" << endl;
+        } else {
+            cout << "Mike" << endl;
+        }
+
+        if (c < min) min = c;
     }
-
-    for (int i = 0; i < n; i++) {
-        int a = (realAi != -1) ? v[realAi] : v[i];
-        int b = v[i + 1];
-
-        if (m[a] == i) cur = a;
-        else if (m[b] == i + 1) cur = b;
-        else if (m[a] - i > m[b] - i + 1) cur = a;
-        else cur = b;
-
-        realAi = (cur == a) ? i + 1 : realAi;
-        if (realAi == -1) realAi = i;
-
-        if (prev == cur) res++;
-        prev = cur;
-    }
-
-    cout << endl << res << endl;
 
     return 0;
 }
+
+/**
+// B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+
+    int n;
+    cin >> n;
+
+    vector<vector<ll>> m(n, vector<ll>(n));
+    vector<ll> a(n);
+
+    for (auto &row : m) {
+        for (auto &item : row) {
+            cin >> item;
+        }
+    }
+
+    a[n - 2] = (ll)sqrt((m[n - 3][n - 2] * m[n - 2][n - 1]) / m[n - 3][n - 1]);
+    a[n - 1] = m[n - 2][n - 1] / a[n - 2];
+    a[n - 3] = m[n - 3][n - 2] / a[n - 2];
+
+    for (int i = n - 4; i >= 0; i--) {
+        a[i] = m[i + 1][i] / a[i + 1];
+    }
+
+    for (auto &item : a) {
+        cout << item << ' ';
+    }
+
+    cout << endl;
+    return 0;
+}
+
+*/
